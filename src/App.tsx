5dfcc26b6a8d3083cd5c1c09794803c3b9cb4550
Lukas Heddendorp <lu.heddendorp@gmail.com>
2021-03-11 17:26:07 +0100

    chore: Add material-ui and set initial theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,44 @@
 import React from 'react';
+import { createMuiTheme, CssBaseline, ThemeProvider } from '@material-ui/core';
+
+const theme = createMuiTheme({
+  typography: {
+    fontFamily: [
+      '-apple-system',
+      'BlinkMacSystemFont',
+      '"Segoe UI"',
+      'Roboto',
+      '"Helvetica Neue"',
+      'Arial',
+      'sans-serif',
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(','),
+  },
+  palette: {
+    primary: {
+      light: '#415176',
+      main: '#122654',
+      dark: '#0c1a3a',
+      contrastText: '#fff',
+    },
+    secondary: {
+      light: '#df4f9b',
+      main: '#D72483',
+      dark: '#96195b',
+      contrastText: '#000',
+    },
+  },
+});
 
 function App() {
-  return <p>Here be an app</p>;
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <p>Here be an app</p>
+    </ThemeProvider>
+  );
 }
 
 export default App;
