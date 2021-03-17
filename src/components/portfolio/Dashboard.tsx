2832719d86f48f3cc67728c2aee4e3e732de1486
Sihan Jennifer Zhao <sihan.jennifer.zhao@gmail.com>
2021-03-17 17:31:19 +0100

    Add Mock APIClient for Portfolio information, Add 2 mock portfolios

--- src/components/portfolio/Dashboard.tsx
+++ src/components/portfolio/Dashboard.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import { CircularProgress } from '@material-ui/core';
+import { MockOverview, MockOverviewTwo, PortfolioOverview } from '../../portfolio/APIClient';
 
 export type DashboardProps = {
   token: string;
+
 };
 
 const Dashboard: React.FC<DashboardProps> = ({ token }) => {
-  const [portfolios, setPortfolios] = React.useState();
+  const [portfolios, setPortfolios] = React.useState<PortfolioOverview[]>();
   React.useEffect(() => {
     // TODO: Fetch portfolio list
-    setPortfolios(undefined);
+    setPortfolios([MockOverview, MockOverviewTwo]);
   }, []);
   return <div>{!portfolios && <CircularProgress />}</div>;
 };
