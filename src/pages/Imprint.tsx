4742c517cb83c240c9e47da41023f26768dcb3fa
Simon Huang <simon.huang@tum.de>
2021-03-19 12:24:06 +0100

    Add imprint page

---
+++ src/pages/Imprint.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+const Imprint: React.FC = () => (
+  <section>
+    <h1>Imprint</h1>
+
+    <section>
+      <h3>In Accordance with Section 5 TMG</h3>
+      <p>
+        Christoph Hanus<br />
+      Max-Bill-Straße 67<br />
+      80807 München
+    </p>
+
+      <h3>Contact</h3>
+      <a href="mailto:info@milou.io">info@milou.io</a>
+    </section>
+
+    <section>
+      <h2>Accountability for Content</h2>
+      <p>
+        The contents of our pages have been created with the utmost care. However, we cannot guarantee the content&apos;s accuracy, completeness or topicality. According to statutory provisions, we are furthermore responsible for our own content on these web pages. In this matter, please note that we are not obliged to monitor the transmitted or saved information of third parties, or investigate circumstances pointing to illegal activity. Our obligations to remove or block the use of information under generally applicable laws remain unaffected by this as per §§ 8 to 10 of the Telemedia Act (TMG).
+      </p>
+    </section>
+
+    <section>
+      <h2>Accountability for Links</h2>
+      <p>Responsibility for the content of external links (to web pages of third parties) lies solely with the operators of the linked pages. No violations were evident to us at the time of linking. Should any legal infringement become known to us, we will remove the respective link immediately.</p>
+    </section>
+
+    <section>
+      <h2>Copyright</h2>
+      <p>Our web pages and their contents are subject to German copyright law. Unless expressly permitted by law, every form of utilizing, reproducing or processing works subject to copyright protection on our web pages requires the prior consent of the respective owner of the rights. Individual reproductions of a work are only allowed for private use. The materials from these pages are copyrighted and any unauthorized use may violate copyright laws.</p>
+    </section>
+  </section>
+);
+
+export default Imprint
