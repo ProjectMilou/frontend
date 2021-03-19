508eca90a1063dbc348798a3966da8b39a83cccd
denispaluca <denispaluca@gmail.com>
2021-03-19 10:24:05 +0100

    Change footer css

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { createStyles, Divider, Grid, makeStyles, SvgIcon, Theme } from '@material-ui/core';
+import { createStyles, Divider, Grid, makeStyles, Theme } from '@material-ui/core';
 import { Link } from '@reach/router';
 import logo from '../../assets/images/logo2.png';
 import Ellipse from './Ellipse';
@@ -8,27 +8,32 @@ const useStyles = makeStyles((theme: Theme) => {
     const { main, light, contrastText } = theme.palette.primary;
     return createStyles({
         logo: {
-            maxWidth: 100,
+            maxWidth: 157,
             marginRight: '10px'
-        },
-        grow: {
-            flexGrow: 1
         },
         link: {
             color: contrastText,
             textDecoration: "none",
             textTransform: "none",
-            margin: 10,
-            fontSize: 15
-        },
-        p: {
-            textAlign: 'center'
+            fontWeight: "bold",
+            fontSize: 12
         },
         container: {
-            backgroundColor: main
+            backgroundColor: main,
+            padding: 50
         },
         divider: {
-            backgroundColor: light
+            backgroundColor: "white"
+        },
+        copyrightText: {
+            color: light,
+            fontSize: 11.5
+        },
+        iconGrid: {
+            padding: 20
+        },
+        linkGrid: {
+            paddingBottom: 20
         }
     })
 });
@@ -37,31 +42,53 @@ const Footer: React.FC = () => {
     const classes = useStyles();
     return (
         <div className={classes.container}>
-            <Grid container direction="row" alignItems="center" >
-                <Grid item xs justify="space-between">
-                    <Link to="/aboutus" className={classes.link}>About us</Link>
-                    <Link to="/mobileapp" className={classes.link}>Mobile App</Link>
+            <Grid
+                container
+                direction="row"
+                alignItems="center"
+                justify="space-between"
+                className={classes.linkGrid}>
+                <Grid container xs={3} justify="flex-start" spacing={8}>
+                    <Grid item>
+                        <Link to="/aboutus" className={classes.link}>About us</Link>
+                    </Grid>
+                    <Grid item>
+                        <Link to="/mobileapp" className={classes.link}>Mobile App</Link>
+                    </Grid>
                 </Grid>
-                <Grid container xs={6} justify="center">
+                <Grid container xs={3} justify="center">
                     <Grid item>
                         <img src={logo} alt="milou-logo" className={classes.logo} />
                     </Grid>
                 </Grid>
 
-                <Grid container xs justify="flex-end">
-                    <Link to="/privacy" className={classes.link}>Privacy Policy</Link>
-                    <Link to="/terms" className={classes.link}>Terms and Conditions</Link>
+                <Grid container xs={3} justify="flex-end" spacing={6} direction="row">
+                    <Grid item>
+                        <Link to="/privacy" className={classes.link}>Privacy Policy</Link>
+                    </Grid>
+                    <Grid item>
+                        <Link to="/terms" className={classes.link}>Terms and Conditions</Link>
+                    </Grid>
                 </Grid>
             </Grid>
+
             <Divider className={classes.divider} />
+
             <Grid
                 container
                 direction="column"
                 justify="space-around"
                 alignItems="center"
             >
-                <Grid item><Ellipse /><Ellipse /><Ellipse /><Ellipse /></Grid>
-                <Grid item>© Photo, Inc. 2019. We love our users!</Grid>
+                <Grid item className={classes.iconGrid}>
+                    <Ellipse />
+                    <Ellipse />
+                    <Ellipse />
+                    <Ellipse />
+                </Grid>
+                <Grid item className={classes.copyrightText}>
+                    © Photo, Inc. 2019. We love our users!
+                </Grid>
             </Grid>
         </div>
     );
