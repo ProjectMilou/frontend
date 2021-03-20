ProjectMilou/frontend

commit a4d6217891923f8e74354da22e3a8d2401f09057

Router (#53)
* Add reach router

* Add Header and NavLink

* Add navlink styling

* Add login register buttons

* Add header test

* Add footer and Ellipse

* Make divider light and send links to end

* Change footer css

* Finish footer design

* Change footer to stick to bottom

* Update tests

* fix: Make the router work in subdirectories (#54)

This makes sure all links resolve to the deployment url

* Add home link to logo

* Add test for links

Co-authored-by: Lukas Heddendorp <lu.heddendorp@gmail.com>

src/components/App.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import { createMuiTheme, CssBaseline, ThemeProvider } from '@material-ui/core';
+import { Router } from '@reach/router';
 import Shell from './shell/Shell';
 import Analyser from './analyser/Analyser';
 import Portfolio from './portfolio/Portfolio';
+import Header from './header/Header';
+import Footer from './footer/Footer';
 
 const theme = createMuiTheme({
   typography: {
@@ -38,11 +41,25 @@ const theme = createMuiTheme({
 const App: React.FC = () => (
   <ThemeProvider theme={theme}>
     <CssBaseline />
-    {/* TODO: Change component hierarchy */}
-    <p>Here be an app</p>
-    <Shell />
-    <Analyser />
-    <Portfolio />
+
+    <div style={{
+      display: 'flex',
+      flexDirection: 'column',
+      minHeight: '100vh'
+    }} >
+      <Header />
+
+      <div style={{ flexGrow: 1 }} >
+        <Router>
+          {/* <Frontpage path="/" /> */}
+          <Shell path="/shell" />
+          <Analyser path="/analyser" />
+          <Portfolio path="/portofolio" />
+        </Router>
+      </div>
+
+      <Footer />
+    </div>
   </ThemeProvider>
 );
 

src/components/header/Header.tsx
@@ -1,0 +1,43 @@
+import { AppBar, Button, createStyles, makeStyles, Theme, Toolbar } from '@material-ui/core';
+import React from 'react';
+import logo from '../../assets/images/logo1.png';
+import NavLink from './NavLink';
+
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        logo: {
+            maxWidth: 100,
+            marginRight: '10px'
+        },
+        grow: {
+            flexGrow: 1
+        },
+        button: {
+            margin: theme.spacing(1)
+        }
+    })
+);
+
+const Header: React.FC = () => {
+    const classes = useStyles();
+    return (
+        <AppBar position="sticky" color="inherit">
+            <Toolbar>
+                <NavLink to="/">
+                    <img src={logo} alt="milou-logo" className={classes.logo} />
+                </NavLink>
+                <Button><NavLink to="/">Home</NavLink></Button>
+                <Button><NavLink to="/portofolio">Portofolio</NavLink></Button>
+                <Button><NavLink to="/analyser">Analyser</NavLink></Button>
+                <Button><NavLink to="/academy">Academy</NavLink></Button>
+
+                <div className={classes.grow} />
+
+                <Button className={classes.button} variant="outlined" color="primary">Login</Button>
+                <Button variant="contained" color="primary">Register</Button>
+            </Toolbar>
+        </AppBar>
+    );
+};
+
+export default Header;

src/components/App.test.tsx
@@ -5,7 +5,7 @@ import App from './App';
 describe('App', () => {
   test('renders first <p>', () => {
     render(<App />);
-    const linkElement = screen.getByText(/Here be an app/i);
+    const linkElement = screen.getByText(/Home/i); //change because 'Here be an app' is not in website
     expect(linkElement).toBeInTheDocument();
   });
 });

src/components/footer/Footer.test.tsx
@@ -1,0 +1,13 @@
+import { render, screen } from '@testing-library/react';
+import Footer from './Footer';
+
+describe('Footer', () => {
+    // TODO: Write actual tests
+    test('Renders all buttons', () => {
+        render(<Footer />);
+        expect(screen.getByText(/About us/i)).toBeInTheDocument();
+        expect(screen.getByText(/Mobile App/i)).toBeInTheDocument();
+        expect(screen.getByText(/Privacy Policy/i)).toBeInTheDocument();
+        expect(screen.getByText(/Terms and Conditions/i)).toBeInTheDocument();
+    });
+});

src/components/header/Header.test.tsx
@@ -1,0 +1,34 @@
+import { fireEvent, render, screen } from '@testing-library/react';
+import Header from './Header';
+
+describe('Header', () => {
+    // TODO: Write actual tests
+    test('Renders all links and buttons', () => {
+        render(<Header />);
+        expect(screen.getByText(/Home/i)).toBeInTheDocument();
+        expect(screen.getByText(/Portofolio/i)).toBeInTheDocument();
+        expect(screen.getByText(/Analyser/i)).toBeInTheDocument();
+        expect(screen.getByText(/Academy/i)).toBeInTheDocument();
+        expect(screen.getByText(/login/i)).toBeInTheDocument();
+        expect(screen.getByText(/Register/i)).toBeInTheDocument();
+    });
+
+    test('All links change the url', () => {
+        render(<Header />);
+        const homeLink = screen.getByText(/Home/i);
+        fireEvent.click(homeLink);
+        expect(location.pathname).toBe('/');
+
+        const portofolioLink = screen.getByText(/Portofolio/i);
+        fireEvent.click(portofolioLink);
+        expect(location.pathname).toBe('/portofolio');
+
+        const analyserLink = screen.getByText(/Analyser/i);
+        fireEvent.click(analyserLink);
+        expect(location.pathname).toBe('/analyser');
+
+        const academyLink = screen.getByText(/Academy/i);
+        fireEvent.click(academyLink);
+        expect(location.pathname).toBe('/academy');
+    });
+});

public/index.html
@@ -1,20 +1,21 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
+    <base href="%PUBLIC_URL%/">
     <meta charset="utf-8" />
-    <link rel="icon" href="%PUBLIC_URL%/favicon.ico" />
+    <link rel="icon" href="./favicon.ico" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta name="theme-color" content="#000000" />
     <meta
       name="description"
       content="Web site created using create-react-app"
     />
-    <link rel="apple-touch-icon" href="%PUBLIC_URL%/logo192.png" />
+    <link rel="apple-touch-icon" href="./logo192.png" />
     <!--
       manifest.json provides metadata used when your web app is installed on a
       user's mobile device or desktop. See https://developers.google.com/web/fundamentals/web-app-manifest/
     -->
-    <link rel="manifest" href="%PUBLIC_URL%/manifest.json" />
+    <link rel="manifest" href="./manifest.json" />
     <!--
       Notice the use of %PUBLIC_URL% in the tags above.
       It will be replaced with the URL of the `public` folder during the build.

commit dc1d84711d757b00456981a9fe074bac1776281a

Fix errors and typos introduced in #53.
- Replace 'portofolio' with 'portfolio'.
- Fix eslint errors.
- Format affected files according to prettier.

src/components/App.tsx
@@ -42,19 +42,21 @@ const App: React.FC = () => (
   <ThemeProvider theme={theme}>
     <CssBaseline />
 
-    <div style={{
-      display: 'flex',
-      flexDirection: 'column',
-      minHeight: '100vh'
-    }} >
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        minHeight: '100vh',
+      }}
+    >
       <Header />
 
-      <div style={{ flexGrow: 1 }} >
+      <div style={{ flexGrow: 1 }}>
         <Router>
           {/* <Frontpage path="/" /> */}
           <Shell path="/shell" />
           <Analyser path="/analyser" />
-          <Portfolio path="/portofolio" />
+          <Portfolio path="/portfolio" />
         </Router>
       </div>
 

src/components/header/Header.tsx
@@ -1,43 +1,62 @@
-import { AppBar, Button, createStyles, makeStyles, Theme, Toolbar } from '@material-ui/core';
+import {
+  AppBar,
+  Button,
+  createStyles,
+  makeStyles,
+  Theme,
+  Toolbar,
+} from '@material-ui/core';
 import React from 'react';
 import logo from '../../assets/images/logo1.png';
 import NavLink from './NavLink';
 
 const useStyles = makeStyles((theme: Theme) =>
-    createStyles({
-        logo: {
-            maxWidth: 100,
-            marginRight: '10px'
-        },
-        grow: {
-            flexGrow: 1
-        },
-        button: {
-            margin: theme.spacing(1)
-        }
-    })
+  createStyles({
+    logo: {
+      maxWidth: 100,
+      marginRight: '10px',
+    },
+    grow: {
+      flexGrow: 1,
+    },
+    button: {
+      margin: theme.spacing(1),
+    },
+  })
 );
 
 const Header: React.FC = () => {
-    const classes = useStyles();
-    return (
-        <AppBar position="sticky" color="inherit">
-            <Toolbar>
-                <NavLink to="/">
-                    <img src={logo} alt="milou-logo" className={classes.logo} />
-                </NavLink>
-                <Button><NavLink to="/">Home</NavLink></Button>
-                <Button><NavLink to="/portofolio">Portofolio</NavLink></Button>
-                <Button><NavLink to="/analyser">Analyser</NavLink></Button>
-                <Button><NavLink to="/academy">Academy</NavLink></Button>
+  const classes = useStyles();
+  return (
+    <AppBar position="sticky" color="inherit">
+      <Toolbar>
+        <NavLink to="/">
+          <img src={logo} alt="milou-logo" className={classes.logo} />
+        </NavLink>
+        <Button>
+          <NavLink to="/">Home</NavLink>
+        </Button>
+        <Button>
+          <NavLink to="/portfolio">Portfolio</NavLink>
+        </Button>
+        <Button>
+          <NavLink to="/analyser">Analyser</NavLink>
+        </Button>
+        <Button>
+          <NavLink to="/academy">Academy</NavLink>
+        </Button>
 
-                <div className={classes.grow} />
+        <div className={classes.grow} />
 
-                <Button className={classes.button} variant="outlined" color="primary">Login</Button>
-                <Button variant="contained" color="primary">Register</Button>
-            </Toolbar>
-        </AppBar>
-    );
+        <Button className={classes.button} variant="outlined" color="primary">
+          Login
+        </Button>
+        <Button variant="contained" color="primary">
+          Register
+        </Button>
+      </Toolbar>
+    </AppBar>
+  );
 };
 
 export default Header;

src/components/App.test.tsx
@@ -5,7 +5,7 @@ import App from './App';
 describe('App', () => {
   test('renders first <p>', () => {
     render(<App />);
-    const linkElement = screen.getByText(/Home/i); //change because 'Here be an app' is not in website
+    const linkElement = screen.getByText(/Home/i);
     expect(linkElement).toBeInTheDocument();
   });
 });

src/components/footer/Footer.test.tsx
@@ -1,13 +1,14 @@
 import { render, screen } from '@testing-library/react';
+import React from 'react';
 import Footer from './Footer';
 
 describe('Footer', () => {
-    // TODO: Write actual tests
-    test('Renders all buttons', () => {
-        render(<Footer />);
-        expect(screen.getByText(/About us/i)).toBeInTheDocument();
-        expect(screen.getByText(/Mobile App/i)).toBeInTheDocument();
-        expect(screen.getByText(/Privacy Policy/i)).toBeInTheDocument();
-        expect(screen.getByText(/Terms and Conditions/i)).toBeInTheDocument();
-    });
+  // TODO: Write actual tests
+  test('Renders all buttons', () => {
+    render(<Footer />);
+    expect(screen.getByText(/About us/i)).toBeInTheDocument();
+    expect(screen.getByText(/Mobile App/i)).toBeInTheDocument();
+    expect(screen.getByText(/Privacy Policy/i)).toBeInTheDocument();
+    expect(screen.getByText(/Terms and Conditions/i)).toBeInTheDocument();
+  });
 });

src/components/header/Header.test.tsx
@@ -1,34 +1,35 @@
 import { fireEvent, render, screen } from '@testing-library/react';
+import React from 'react';
 import Header from './Header';
 
 describe('Header', () => {
-    // TODO: Write actual tests
-    test('Renders all links and buttons', () => {
-        render(<Header />);
-        expect(screen.getByText(/Home/i)).toBeInTheDocument();
-        expect(screen.getByText(/Portofolio/i)).toBeInTheDocument();
-        expect(screen.getByText(/Analyser/i)).toBeInTheDocument();
-        expect(screen.getByText(/Academy/i)).toBeInTheDocument();
-        expect(screen.getByText(/login/i)).toBeInTheDocument();
-        expect(screen.getByText(/Register/i)).toBeInTheDocument();
-    });
+  // TODO: Write actual tests
+  test('Renders all links and buttons', () => {
+    render(<Header />);
+    expect(screen.getByText(/Home/i)).toBeInTheDocument();
+    expect(screen.getByText(/Portfolio/i)).toBeInTheDocument();
+    expect(screen.getByText(/Analyser/i)).toBeInTheDocument();
+    expect(screen.getByText(/Academy/i)).toBeInTheDocument();
+    expect(screen.getByText(/login/i)).toBeInTheDocument();
+    expect(screen.getByText(/Register/i)).toBeInTheDocument();
+  });
 
-    test('All links change the url', () => {
-        render(<Header />);
-        const homeLink = screen.getByText(/Home/i);
-        fireEvent.click(homeLink);
-        expect(location.pathname).toBe('/');
+  test('All links change the url', () => {
+    render(<Header />);
+    const homeLink = screen.getByText(/Home/i);
+    fireEvent.click(homeLink);
+    expect(window.location.pathname).toBe('/');
 
-        const portofolioLink = screen.getByText(/Portofolio/i);
-        fireEvent.click(portofolioLink);
-        expect(location.pathname).toBe('/portofolio');
+    const portfolioLink = screen.getByText(/Portfolio/i);
+    fireEvent.click(portfolioLink);
+    expect(window.location.pathname).toBe('/portfolio');
 
-        const analyserLink = screen.getByText(/Analyser/i);
-        fireEvent.click(analyserLink);
-        expect(location.pathname).toBe('/analyser');
+    const analyserLink = screen.getByText(/Analyser/i);
+    fireEvent.click(analyserLink);
+    expect(window.location.pathname).toBe('/analyser');
 
-        const academyLink = screen.getByText(/Academy/i);
-        fireEvent.click(academyLink);
-        expect(location.pathname).toBe('/academy');
-    });
+    const academyLink = screen.getByText(/Academy/i);
+    fireEvent.click(academyLink);
+    expect(window.location.pathname).toBe('/academy');
+  });
 });