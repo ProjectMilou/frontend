4cec5f9d0ec37fbd594780656df50b72abf1399e
Simon Huang <simon.huang@tum.de>
2021-03-21 12:45:36 +0100

    refactor imprint to use MaterialUI components

--- src/pages/Imprint.tsx
+++ src/pages/Imprint.tsx
@@ -1,38 +1,66 @@
 import React from 'react';
+import { Box, Typography } from '@material-ui/core';
 
 const Imprint: React.FC = () => (
-  <section>
-    <h1>Imprint</h1>
+  <Box>
+    <Typography variant="h2">Imprint</Typography>
 
-    <section>
-      <h3>In Accordance with Section 5 TMG</h3>
-      <p>
-        Christoph Hanus<br />
-      Max-Bill-Straße 67<br />
-      80807 München
-    </p>
+    <Box>
+      <Typography variant="h4">In Accordance with Section 5 TMG</Typography>
+      <Typography variant="body1">
+        Christoph Hanus
+        <br />
+        Max-Bill-Straße 67
+        <br />
+        80807 München
+      </Typography>
 
-      <h3>Contact</h3>
-      <a href="mailto:info@milou.io">info@milou.io</a>
-    </section>
+      <Typography variant="h4">Contact</Typography>
 
-    <section>
-      <h2>Accountability for Content</h2>
-      <p>
-        The contents of our pages have been created with the utmost care. However, we cannot guarantee the content&apos;s accuracy, completeness or topicality. According to statutory provisions, we are furthermore responsible for our own content on these web pages. In this matter, please note that we are not obliged to monitor the transmitted or saved information of third parties, or investigate circumstances pointing to illegal activity. Our obligations to remove or block the use of information under generally applicable laws remain unaffected by this as per §§ 8 to 10 of the Telemedia Act (TMG).
-      </p>
-    </section>
+      <Typography variant="body1">
+        <a href="mailto:info@milou.io">info@milou.io</a>
+      </Typography>
+    </Box>
 
-    <section>
-      <h2>Accountability for Links</h2>
-      <p>Responsibility for the content of external links (to web pages of third parties) lies solely with the operators of the linked pages. No violations were evident to us at the time of linking. Should any legal infringement become known to us, we will remove the respective link immediately.</p>
-    </section>
+    <Box>
+      <Typography variant="h3">Accountability for Content</Typography>
+      <Typography variant="body1">
+        The contents of our pages have been created with the utmost care.
+        However, we cannot guarantee the content&apos;s accuracy, completeness
+        or topicality. According to statutory provisions, we are furthermore
+        responsible for our own content on these web pages. In this matter,
+        please note that we are not obliged to monitor the transmitted or saved
+        information of third parties, or investigate circumstances pointing to
+        illegal activity. Our obligations to remove or block the use of
+        information under generally applicable laws remain unaffected by this as
+        per §§ 8 to 10 of the Telemedia Act (TMG).
+      </Typography>
+    </Box>
 
-    <section>
-      <h2>Copyright</h2>
-      <p>Our web pages and their contents are subject to German copyright law. Unless expressly permitted by law, every form of utilizing, reproducing or processing works subject to copyright protection on our web pages requires the prior consent of the respective owner of the rights. Individual reproductions of a work are only allowed for private use. The materials from these pages are copyrighted and any unauthorized use may violate copyright laws.</p>
-    </section>
-  </section>
+    <Box>
+      <Typography variant="h3">Accountability for Links</Typography>
+      <Typography variant="body1">
+        Responsibility for the content of external links (to web pages of third
+        parties) lies solely with the operators of the linked pages. No
+        violations were evident to us at the time of linking. Should any legal
+        infringement become known to us, we will remove the respective link
+        immediately.
+      </Typography>
+    </Box>
+
+    <Box>
+      <Typography variant="h3">Copyright</Typography>
+      <Typography variant="body1">
+        Our web pages and their contents are subject to German copyright law.
+        Unless expressly permitted by law, every form of utilizing, reproducing
+        or processing works subject to copyright protection on our web pages
+        requires the prior consent of the respective owner of the rights.
+        Individual reproductions of a work are only allowed for private use. The
+        materials from these pages are copyrighted and any unauthorized use may
+        violate copyright laws.
+      </Typography>
+    </Box>
+  </Box>
 );
 
-export default Imprint
+export default Imprint;
