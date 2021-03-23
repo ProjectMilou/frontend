43abd4a1554f331cb05ee9b29a19065d2a748f24
Simon Huang <simon.huang@tum.de>
2021-03-23 12:08:35 +0100

    simplify nesting

--- src/pages/Imprint.tsx
+++ src/pages/Imprint.tsx
@@ -31,51 +31,51 @@ const Imprint: React.FC<RouteComponentProps> = () => (
           </Typography>
         </Box>
       </Box>
+    </Box>
 
-      <Box my={5}>
-        <Typography variant="h3" gutterBottom>
-          Accountability for Content
-        </Typography>
-        <Typography variant="body1">
-          The contents of our pages have been created with the utmost care.
-          However, we cannot guarantee the content&apos;s accuracy, completeness
-          or topicality. According to statutory provisions, we are furthermore
-          responsible for our own content on these web pages. In this matter,
-          please note that we are not obliged to monitor the transmitted or
-          saved information of third parties, or investigate circumstances
-          pointing to illegal activity. Our obligations to remove or block the
-          use of information under generally applicable laws remain unaffected
-          by this as per §§ 8 to 10 of the Telemedia Act (TMG).
-        </Typography>
-      </Box>
+    <Box my={5}>
+      <Typography variant="h3" gutterBottom>
+        Accountability for Content
+      </Typography>
+      <Typography variant="body1">
+        The contents of our pages have been created with the utmost care.
+        However, we cannot guarantee the content&apos;s accuracy, completeness
+        or topicality. According to statutory provisions, we are furthermore
+        responsible for our own content on these web pages. In this matter,
+        please note that we are not obliged to monitor the transmitted or saved
+        information of third parties, or investigate circumstances pointing to
+        illegal activity. Our obligations to remove or block the use of
+        information under generally applicable laws remain unaffected by this as
+        per §§ 8 to 10 of the Telemedia Act (TMG).
+      </Typography>
+    </Box>
 
-      <Box my={5}>
-        <Typography variant="h3" gutterBottom>
-          Accountability for Links
-        </Typography>
-        <Typography variant="body1">
-          Responsibility for the content of external links (to web pages of
-          third parties) lies solely with the operators of the linked pages. No
-          violations were evident to us at the time of linking. Should any legal
-          infringement become known to us, we will remove the respective link
-          immediately.
-        </Typography>
-      </Box>
+    <Box my={5}>
+      <Typography variant="h3" gutterBottom>
+        Accountability for Links
+      </Typography>
+      <Typography variant="body1">
+        Responsibility for the content of external links (to web pages of third
+        parties) lies solely with the operators of the linked pages. No
+        violations were evident to us at the time of linking. Should any legal
+        infringement become known to us, we will remove the respective link
+        immediately.
+      </Typography>
+    </Box>
 
-      <Box my={5}>
-        <Typography variant="h3" gutterBottom>
-          Copyright
-        </Typography>
-        <Typography variant="body1">
-          Our web pages and their contents are subject to German copyright law.
-          Unless expressly permitted by law, every form of utilizing,
-          reproducing or processing works subject to copyright protection on our
-          web pages requires the prior consent of the respective owner of the
-          rights. Individual reproductions of a work are only allowed for
-          private use. The materials from these pages are copyrighted and any
-          unauthorized use may violate copyright laws.
-        </Typography>
-      </Box>
+    <Box my={5}>
+      <Typography variant="h3" gutterBottom>
+        Copyright
+      </Typography>
+      <Typography variant="body1">
+        Our web pages and their contents are subject to German copyright law.
+        Unless expressly permitted by law, every form of utilizing, reproducing
+        or processing works subject to copyright protection on our web pages
+        requires the prior consent of the respective owner of the rights.
+        Individual reproductions of a work are only allowed for private use. The
+        materials from these pages are copyrighted and any unauthorized use may
+        violate copyright laws.
+      </Typography>
     </Box>
   </Container>
 );
