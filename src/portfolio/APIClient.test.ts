ProjectMilou/frontend

commit fd60a52f48d534e99d972c3add153d24546bd247

feat: Add login and register mocks
* feat: basic login and user registration #6

* feat: add register-tests

* fix: adjust en.json

* fix: delete alert

* Update src/locales/en.json

Co-authored-by: Lukas Heddendorp <lu.heddendorp@gmail.com>

src/components/header/Header.test.tsx
@@ -1,16 +1,35 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import React from 'react';
 import Header from './Header';
 
 describe('Header', () => {
-  // TODO: Write actual tests
-  test('Renders all buttons', () => {
-    render(<Header />);
-    expect(screen.getByText(/Home/i)).toBeInTheDocument();
-    expect(screen.getByText(/Portfolio/i)).toBeInTheDocument();
-    expect(screen.getByText(/Analyser/i)).toBeInTheDocument();
-    expect(screen.getByText(/Academy/i)).toBeInTheDocument();
-    expect(screen.getByText(/login/i)).toBeInTheDocument();
-    expect(screen.getByText(/Register/i)).toBeInTheDocument();
-  });
+    // TODO: Write actual tests
+    test('Renders all links and buttons', () => {
+        render(<Header />);
+        expect(screen.getByText(/Home/i)).toBeInTheDocument();
+        expect(screen.getByText(/Portfolio/i)).toBeInTheDocument();
+        expect(screen.getByText(/Analyser/i)).toBeInTheDocument();
+        expect(screen.getByText(/Academy/i)).toBeInTheDocument();
+        expect(screen.getByText(/login/i)).toBeInTheDocument();
+        expect(screen.getByText(/Register/i)).toBeInTheDocument();
+    });
+
+    test('All links change the url', () => {
+        render(<Header />);
+        const homeLink = screen.getByText(/Home/i);
+        fireEvent.click(homeLink);
+        expect(location.pathname).toBe('/');
+
+        const portfolioLink = screen.getByText(/Portfolio/i);
+        fireEvent.click(portfolioLink);
+        expect(location.pathname).toBe('/portfolio');
+
+        const analyserLink = screen.getByText(/Analyser/i);
+        fireEvent.click(analyserLink);
+        expect(location.pathname).toBe('/analyser');
+
+        const academyLink = screen.getByText(/Academy/i);
+        fireEvent.click(academyLink);
+        expect(location.pathname).toBe('/academy');
+    });
 });

src/components/shell/login/Login.test.tsx
@@ -1,0 +1,73 @@
+import { render, fireEvent } from '@testing-library/react';
+import * as React from 'react';
+import Login from './Login';
+import userEvent from '@testing-library/user-event';
+
+const renderLoginForm = (closePopUp = jest.fn() ) => {
+  const openRegisterPopUp = () => {};
+  return render(
+    <Login closePopUp={closePopUp} openRegisterPopUp={openRegisterPopUp} />
+  );
+};
+
+describe('Login', () => {
+  test('should render email and password input', async () => {
+    const { findByTestId } = renderLoginForm();
+
+    const passwordInput = await findByTestId('password');
+    const emailInput = await findByTestId('email');
+
+    expect(passwordInput).toBeInTheDocument();
+    expect(emailInput).toBeInTheDocument();
+  });
+
+  test('should put in text and enable submit-button', async () => {
+    const { findByTestId } = renderLoginForm();
+
+    const passwordInput = (await findByTestId('password')) as HTMLInputElement;
+    const emailInput = (await findByTestId('email')) as HTMLInputElement;
+    const button = (await findByTestId('login')) as HTMLButtonElement;
+
+    fireEvent.change(emailInput, { target: { value: 'email' } });
+    fireEvent.change(passwordInput, { target: { value: 'pwd' } });
+
+    expect(emailInput.value).toBe('email');
+    expect(passwordInput.value).toBe('pwd');
+    expect(button).toBeEnabled();
+    expect(emailInput.getAttribute("aria-invalid")).toBe("false");
+    expect(passwordInput.getAttribute("aria-invalid")).toBe("false");
+  });
+
+  test('should disable submit-button', async () => {
+    const { findByTestId } = renderLoginForm();
+
+    const passwordInput = (await findByTestId('password')) as HTMLInputElement;
+    const emailInput = (await findByTestId('email')) as HTMLInputElement;
+    const button = (await findByTestId('login')) as HTMLButtonElement;
+
+    expect(emailInput.getAttribute("aria-invalid")).toBe("false");
+
+    // both inputs empty
+    expect(button).toBeDisabled();
+
+    // password input empty
+    userEvent.type(emailInput, "email")
+    expect(button).toBeDisabled();
+
+    // email input empty
+    fireEvent.change(emailInput, { target: { value: '' } });
+    fireEvent.change(passwordInput, { target: { value: 'pwd' } });
+    expect(button).toBeDisabled();
+  });
+
+  test('should close popup window', async () => {
+    const closePopUp = jest.fn()
+    const { findByTestId } = renderLoginForm(closePopUp);
+
+    const icon = (await findByTestId('icon'));
+    userEvent.click(icon)
+    expect(closePopUp).toHaveBeenCalledTimes(1);
+  });
+
+  // Tests for mock user login (success and error)
+});

src/components/header/Header.tsx
@@ -1,14 +1,17 @@
+import React, { useState } from 'react';
 import {
   AppBar,
   Button,
-  createStyles,
+  createStyles,    
+  Dialog,
   makeStyles,
   Theme,
   Toolbar,
 } from '@material-ui/core';
-import React from 'react';
 import logo from '../../assets/images/logo1.png';
 import NavLink from './NavLink';
+import Login from '../shell/login/Login';
+import Register from '../shell/register/Register';
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
@@ -21,34 +24,53 @@ const useStyles = makeStyles((theme: Theme) =>
         },
         button: {
             margin: theme.spacing(1)
+        },
+        dialog: {
+            borderRadius: "10px",
+            maxWidth: "450px",
+            margin: "auto"
         }
     })
 );
 
 const Header: React.FC = () => {
-  const classes = useStyles();
-  return (
-    <AppBar position="sticky" color="inherit">
-      <Toolbar>
-        <img src={logo} alt="milou-logo" className={classes.logo} />
+    const classes = useStyles();
+    const [openLogin, setOpenLogin] = useState(false);
+    const [openRegister, setOpenRegister] = useState(false);
 
-        <Button>
-          <NavLink to="/">Home</NavLink>
-        </Button>
-        <Button>
-          <NavLink to="/portfolio">Portfolio</NavLink>
-        </Button>
-        <Button>
-          <NavLink to="/analyser">Analyser</NavLink>
-        </Button>
-        <Button>
-          <NavLink to="/academy">Academy</NavLink>
-        </Button>
+  return (
+        <AppBar position="sticky" color="inherit">
+            <Toolbar>
+                <NavLink to="/">
+                    <img src={logo} alt="milou-logo" className={classes.logo} />
+                </NavLink>
+                <Button><NavLink to="/">Home</NavLink></Button>
+                <Button><NavLink to="/portfolio">Portfolio</NavLink></Button>
+                <Button><NavLink to="/analyser">Analyser</NavLink></Button>
+                <Button><NavLink to="/academy">Academy</NavLink></Button>
 
                 <div className={classes.grow} />
 
-                <Button className={classes.button} variant="outlined" color="primary">Login</Button>
-                <Button variant="contained" color="primary">Register</Button>
+                <Button className={classes.button} variant="outlined" color="primary" onClick={() => setOpenLogin(true)}>Login</Button>
+                <Button variant="contained" color="primary" onClick={() => setOpenRegister(true)}>Register</Button>
+                <Dialog open={openLogin} onClose={() => setOpenLogin(false)} className={classes.dialog}>
+                  <Login
+                    closePopUp={() => setOpenLogin(false)}
+                    openRegisterPopUp={() => {
+                      setOpenLogin(false);
+                      setOpenRegister(true);
+                    }}
+                  />
+                </Dialog>
+              <Dialog open={openRegister} onClose={() => setOpenRegister(false)} className={classes.dialog}>
+                <Register
+                  closePopUp={() => setOpenRegister(false)}
+                  openLoginPopUp={() => {
+                    setOpenRegister(false);
+                    setOpenLogin(true);
+                  }}
+                />
+              </Dialog>
             </Toolbar>
         </AppBar>
     );

commit fd81206297fa76fa1f868129178c4e23f90c7476

Portfolio API client (#84)
Implement the API client with 'list' and 'rename' methods. Other methods can easily be added when required.

* Implement portfolio API client.
* Add portfolio error messages.
* Fix eslint errors introduced in #71.

src/components/portfolio/Dashboard.tsx
@@ -34,7 +34,7 @@ const Dashboard: React.FC<DashboardProps> = ({ token, selectPortfolio }) => {
   const fetch = async () => {
     setError(undefined);
     try {
-      const p = await API.portfolioOverview(token);
+      const p = await API.list(token);
       if (isMounted.current) {
         setPortfolios(p);
       }

src/portfolio/APIClient.ts
@@ -1,4 +1,14 @@
+// TODO: Change to production URL when available
+// TODO: Use a common base URL for shell, analyser and portfolio instead of 3 URLs
+export const baseURL =
+  'http://milou-env.eba-98uqk5ec.eu-central-1.elasticbeanstalk.com/portfolio';
+
+const headers = { 'Content-Type': 'application/json' };
+
+// Types used by the frontend
+
 export type PortfolioOverview = {
+  // TODO: Change id to number to be consistent with backend
   id: string;
   name: string;
   virtual: boolean;
@@ -10,39 +20,83 @@ export type PortfolioOverview = {
   modified: Date;
 };
 
-// TODO: Move mocks to test files once the APIClient is implemented
+// Types describing the JSON response of API calls.
+// The correctness of these types is assumed, no checks are performed.
 
-export const MockOverview: PortfolioOverview = {
-  id: '1',
-  name: 'test',
-  virtual: true,
-  positionCount: 3,
-  value: 9,
-  score: undefined,
-  perf7d: -1.23,
-  perf1y: 13.37,
-  modified: new Date(),
+type ListResponse = {
+  portfolios: {
+    id: number;
+    name: string;
+    virtual: boolean;
+    positionCount: number;
+    value: number;
+    score?: number;
+    perf7d: number;
+    perf1y: number;
+    /** UNIX timestamp */
+    modified: number;
+  }[];
 };
 
-export const MockOverviewTwo: PortfolioOverview = {
-  id: '2',
-  name: 'testTwo',
-  virtual: false,
-  positionCount: 4,
-  value: 3,
-  score: 20,
-  perf7d: 0,
-  perf1y: -1,
-  modified: new Date(),
-};
+/**
+ * Makes an API call. Resolves to the JSON response if the call is successful,
+ * otherwise rejects with an error that has an {@link ErrorCode} as message.
+ *
+ * @param token - Authentication token
+ * @param method - Request method (GET, POST, etc.)
+ * @param url - An URL relative to {@link baseURL}
+ * @param body - The request body
+ * @param additionalHeaders - Additional request headers
+ *
+ * @return Parsed JSON response if the API call succeeds
+ */
+async function request(
+  token: string,
+  method: string,
+  url: string,
+  body?: string,
+  additionalHeaders?: HeadersInit
+): Promise<unknown> {
+  // TODO: authentication
+  const response = await fetch(`${baseURL}/${url}`, {
+    method,
+    headers: { ...headers, ...additionalHeaders },
+    body,
+  }).catch(() => Promise.reject(new Error('UNKNOWN'))); // network error etc.
+  if (response.ok) {
+    return Promise.resolve(response.json()); // valid response
+  }
+  const json = await response
+    .json()
+    .catch(() => Promise.reject(new Error('UNKNOWN'))); // server error without JSON response
+  return Promise.reject(new Error(json.error)); // JSON error
+}
 
-export async function portfolioOverview(
-  token: string // eslint-disable-line @typescript-eslint/no-unused-vars
-): Promise<PortfolioOverview[]> {
-  // TODO: implement API call
-  return new Promise((resolve) =>
-    setTimeout(() => {
-      resolve([MockOverview, MockOverviewTwo]);
-    }, 500)
-  );
+/**
+ * Gets an overview over all portfolios of the authenticated user.
+ *
+ * @param token - Authentication token
+ */
+export async function list(token: string): Promise<PortfolioOverview[]> {
+  const response = (await request(token, 'GET', 'list')) as ListResponse;
+  return response.portfolios.map((p) => ({
+    ...p,
+    id: p.id.toString(),
+    modified: new Date(p.modified),
+  }));
+}
+
+/**
+ * Renames a portfolio.
+ *
+ * @param token - Authentication token
+ * @param id - ID of the portfolio to be renamed
+ * @param name - New name of the portfolio
+ */
+export async function rename(
+  token: string,
+  id: string,
+  name: string
+): Promise<void> {
+  await request(token, 'PUT', `rename/${id}`, JSON.stringify({ name }));
 }

src/portfolio/APIMocks.ts
@@ -1,0 +1,35 @@
+/**
+ * This module contains mocks for objects that are usually fetched from the API
+ * for use in tests. These mocks are required by multiple tests, but should not
+ * be used outside of tests. Ideally, the mocks would be in 'APIClient.test.ts',
+ * but including a test file from other files causes the test script to execute
+ * the tests multiple times.
+ *
+ * TL;DR: Never import this file, except for tests.
+ */
+
+import { PortfolioOverview } from './APIClient';
+
+export const MockOverview: PortfolioOverview = {
+  id: '1',
+  name: 'test',
+  virtual: true,
+  positionCount: 3,
+  value: 9,
+  score: undefined,
+  perf7d: -1.23,
+  perf1y: 13.37,
+  modified: new Date(0),
+};
+
+export const MockOverviewTwo: PortfolioOverview = {
+  id: '2',
+  name: 'testTwo',
+  virtual: false,
+  positionCount: 4,
+  value: 3,
+  score: 20,
+  perf7d: 0,
+  perf1y: -1,
+  modified: new Date(1616086585),
+};

src/components/header/Header.test.tsx
@@ -3,33 +3,33 @@ import React from 'react';
 import Header from './Header';
 
 describe('Header', () => {
-    // TODO: Write actual tests
-    test('Renders all links and buttons', () => {
-        render(<Header />);
-        expect(screen.getByText(/Home/i)).toBeInTheDocument();
-        expect(screen.getByText(/Portfolio/i)).toBeInTheDocument();
-        expect(screen.getByText(/Analyser/i)).toBeInTheDocument();
-        expect(screen.getByText(/Academy/i)).toBeInTheDocument();
-        expect(screen.getByText(/login/i)).toBeInTheDocument();
-        expect(screen.getByText(/Register/i)).toBeInTheDocument();
-    });
+  // TODO: Write actual tests
+  test('Renders all links and buttons', () => {
+    render(<Header />);
+    expect(screen.getByText(/Home/i)).toBeInTheDocument();
+    expect(screen.getByText(/Portfolio/i)).toBeInTheDocument();
+    expect(screen.getByText(/Analyser/i)).toBeInTheDocument();
+    expect(screen.getByText(/Academy/i)).toBeInTheDocument();
+    expect(screen.getByText(/login/i)).toBeInTheDocument();
+    expect(screen.getByText(/Register/i)).toBeInTheDocument();
+  });
 
-    test('All links change the url', () => {
-        render(<Header />);
-        const homeLink = screen.getByText(/Home/i);
-        fireEvent.click(homeLink);
-        expect(location.pathname).toBe('/');
+  test('All links change the url', () => {
+    render(<Header />);
+    const homeLink = screen.getByText(/Home/i);
+    fireEvent.click(homeLink);
+    expect(window.location.pathname).toBe('/');
 
-        const portfolioLink = screen.getByText(/Portfolio/i);
-        fireEvent.click(portfolioLink);
-        expect(location.pathname).toBe('/portfolio');
+    const portfolioLink = screen.getByText(/Portfolio/i);
+    fireEvent.click(portfolioLink);
+    expect(window.location.pathname).toBe('/portfolio');
 
-        const analyserLink = screen.getByText(/Analyser/i);
-        fireEvent.click(analyserLink);
-        expect(location.pathname).toBe('/analyser');
+    const analyserLink = screen.getByText(/Analyser/i);
+    fireEvent.click(analyserLink);
+    expect(window.location.pathname).toBe('/analyser');
 
-        const academyLink = screen.getByText(/Academy/i);
-        fireEvent.click(academyLink);
-        expect(location.pathname).toBe('/academy');
-    });
+    const academyLink = screen.getByText(/Academy/i);
+    fireEvent.click(academyLink);
+    expect(window.location.pathname).toBe('/academy');
+  });
 });

src/components/portfolio/Dashboard.test.tsx
@@ -2,6 +2,7 @@ import { render, fireEvent, act, waitFor } from '@testing-library/react';
 import * as React from 'react';
 import * as API from '../../portfolio/APIClient';
 import Dashboard, { DashboardProps } from './Dashboard';
+import { MockOverview, MockOverviewTwo } from '../../portfolio/APIMocks';
 
 jest.mock('../../portfolio/APIClient');
 const mockAPI = API as jest.Mocked<typeof API>;
@@ -22,9 +23,7 @@ describe('Dashboard', () => {
 
   test('shows loading indicator', async () => {
     // keep loading forever
-    mockAPI.portfolioOverview.mockImplementation(
-      async () => new Promise(() => {})
-    );
+    mockAPI.list.mockImplementation(async () => new Promise(() => {}));
     const { container, queryByText } = renderComponent();
 
     // only the loading indeicator should be visible
@@ -34,9 +33,9 @@ describe('Dashboard', () => {
   });
 
   test('shows portfolios', async () => {
-    const mockPortfolioOverview = mockAPI.portfolioOverview.mockResolvedValue([
-      API.MockOverview,
-      API.MockOverviewTwo,
+    const mockPortfolioOverview = mockAPI.list.mockResolvedValue([
+      MockOverview,
+      MockOverviewTwo,
     ]);
     const { container, queryByText } = renderComponent();
     await act(async () => {
@@ -49,13 +48,13 @@ describe('Dashboard', () => {
     // the portfolio overview should be visible
     expect(queryByText('portfolio.dashboard.title')).toBeInTheDocument();
     expect(container.querySelector('.MuiLinearProgress-bar')).toBeNull();
-    expect(queryByText(API.MockOverview.name)).toBeInTheDocument();
-    expect(queryByText(API.MockOverviewTwo.name)).toBeInTheDocument();
+    expect(queryByText(MockOverview.name)).toBeInTheDocument();
+    expect(queryByText(MockOverviewTwo.name)).toBeInTheDocument();
     expect(queryByText('portfolio.dashboard.createPortfolio')).not.toBeNull();
   });
 
   test('shows error message and loads portfolios again on retry', async () => {
-    let mockPortfolioOverview = mockAPI.portfolioOverview.mockRejectedValue(
+    let mockPortfolioOverview = mockAPI.list.mockRejectedValue(
       new Error('UNKNOWN')
     );
     const { container, queryByText, getByText } = renderComponent();
@@ -73,9 +72,9 @@ describe('Dashboard', () => {
     expect(queryByText('error.message.UNKNOWN')).toBeInTheDocument();
 
     // click retry
-    mockPortfolioOverview = mockAPI.portfolioOverview.mockResolvedValue([
-      API.MockOverview,
-      API.MockOverviewTwo,
+    mockPortfolioOverview = mockAPI.list.mockResolvedValue([
+      MockOverview,
+      MockOverviewTwo,
     ]);
     fireEvent.click(getByText('error.action.retry'));
     await act(async () => {
@@ -89,8 +88,8 @@ describe('Dashboard', () => {
     expect(queryByText('error.message.UNKNOWN')).toBeNull();
     expect(queryByText('portfolio.dashboard.title')).toBeInTheDocument();
     expect(container.querySelector('.MuiLinearProgress-bar')).toBeNull();
-    expect(queryByText(API.MockOverview.name)).toBeInTheDocument();
-    expect(queryByText(API.MockOverviewTwo.name)).toBeInTheDocument();
+    expect(queryByText(MockOverview.name)).toBeInTheDocument();
+    expect(queryByText(MockOverviewTwo.name)).toBeInTheDocument();
     expect(queryByText('portfolio.dashboard.createPortfolio')).not.toBeNull();
   });
 });

src/components/portfolio/DashboardTable.test.tsx
@@ -1,11 +1,11 @@
 import { render, fireEvent } from '@testing-library/react';
 import * as React from 'react';
-import * as API from '../../portfolio/APIClient';
 import DashboardTable, { DashboardTableProps } from './DashboardTable';
+import { MockOverview, MockOverviewTwo } from '../../portfolio/APIMocks';
 
 describe('DashboardTable', () => {
   const defaultProps: DashboardTableProps = {
-    portfolios: [API.MockOverview, API.MockOverviewTwo],
+    portfolios: [MockOverview, MockOverviewTwo],
     selectPortfolio: jest.fn(),
     renamePortfolio: jest.fn(),
     duplicatePortfolio: jest.fn(),

src/components/portfolio/PortfolioOverview.test.tsx
@@ -1,7 +1,7 @@
 import { render, fireEvent } from '@testing-library/react';
 import * as React from 'react';
 import PortfolioOverview, { PortfolioOverviewProps } from './PortfolioOverview';
-import { MockOverview, MockOverviewTwo } from '../../portfolio/APIClient';
+import { MockOverview, MockOverviewTwo } from '../../portfolio/APIMocks';
 
 jest.mock('./DashboardTable', () => () => <table />);
 

src/components/shell/login/Login.test.tsx
@@ -1,9 +1,9 @@
 import { render, fireEvent } from '@testing-library/react';
 import * as React from 'react';
-import Login from './Login';
 import userEvent from '@testing-library/user-event';
+import Login from './Login';
 
-const renderLoginForm = (closePopUp = jest.fn() ) => {
+const renderLoginForm = (closePopUp = jest.fn()) => {
   const openRegisterPopUp = () => {};
   return render(
     <Login closePopUp={closePopUp} openRegisterPopUp={openRegisterPopUp} />
@@ -34,8 +34,8 @@ describe('Login', () => {
     expect(emailInput.value).toBe('email');
     expect(passwordInput.value).toBe('pwd');
     expect(button).toBeEnabled();
-    expect(emailInput.getAttribute("aria-invalid")).toBe("false");
-    expect(passwordInput.getAttribute("aria-invalid")).toBe("false");
+    expect(emailInput.getAttribute('aria-invalid')).toBe('false');
+    expect(passwordInput.getAttribute('aria-invalid')).toBe('false');
   });
 
   test('should disable submit-button', async () => {
@@ -45,13 +45,13 @@ describe('Login', () => {
     const emailInput = (await findByTestId('email')) as HTMLInputElement;
     const button = (await findByTestId('login')) as HTMLButtonElement;
 
-    expect(emailInput.getAttribute("aria-invalid")).toBe("false");
+    expect(emailInput.getAttribute('aria-invalid')).toBe('false');
 
     // both inputs empty
     expect(button).toBeDisabled();
 
     // password input empty
-    userEvent.type(emailInput, "email")
+    userEvent.type(emailInput, 'email');
     expect(button).toBeDisabled();
 
     // email input empty
@@ -61,11 +61,11 @@ describe('Login', () => {
   });
 
   test('should close popup window', async () => {
-    const closePopUp = jest.fn()
+    const closePopUp = jest.fn();
     const { findByTestId } = renderLoginForm(closePopUp);
 
-    const icon = (await findByTestId('icon'));
-    userEvent.click(icon)
+    const icon = await findByTestId('icon');
+    userEvent.click(icon);
     expect(closePopUp).toHaveBeenCalledTimes(1);
   });
 

src/portfolio/APIClient.test.ts
@@ -1,0 +1,87 @@
+import { list, rename } from './APIClient';
+import { MockOverview, MockOverviewTwo } from './APIMocks';
+
+describe('Portfolio API client', () => {
+  beforeEach(() => {
+    fetchMock.resetMocks();
+  });
+
+  /**
+   * Error handling tests that should be executed for each API call.
+   *
+   * @param apiCall The API call to be tested
+   */
+  const errorHandlingTests = (apiCall: () => Promise<unknown>) => {
+    test('rejects on error with non-JSON response', async () => {
+      fetchMock.mockResponseOnce('This is not JSON.', { status: 400 });
+      await expect(apiCall()).rejects.toMatchInlineSnapshot(`[Error: UNKNOWN]`);
+    });
+
+    test('rejects on failed request', async () => {
+      fetchMock.mockRejectOnce(() =>
+        Promise.reject(new Error('network error'))
+      );
+      await expect(apiCall()).rejects.toMatchInlineSnapshot(`[Error: UNKNOWN]`);
+    });
+
+    test('rejects with JSON error', async () => {
+      fetchMock.mockResponseOnce(
+        JSON.stringify({ error: 'AUTH_TOKEN_INVALID' }),
+        { status: 400 }
+      );
+      await expect(apiCall()).rejects.toMatchInlineSnapshot(
+        `[Error: AUTH_TOKEN_INVALID]`
+      );
+    });
+  };
+
+  describe('list', () => {
+    const apiCall = () => list('');
+
+    test('returns portfolios on success', async () => {
+      fetchMock.mockResponseOnce(
+        JSON.stringify({
+          portfolios: [
+            {
+              id: 1,
+              name: 'test',
+              virtual: true,
+              positionCount: 3,
+              value: 9,
+              perf7d: -1.23,
+              perf1y: 13.37,
+              modified: 0,
+            },
+            {
+              id: 2,
+              name: 'testTwo',
+              virtual: false,
+              positionCount: 4,
+              value: 3,
+              score: 20,
+              perf7d: 0,
+              perf1y: -1,
+              modified: 1616086585,
+            },
+          ],
+        })
+      );
+      await expect(apiCall()).resolves.toEqual([MockOverview, MockOverviewTwo]);
+    });
+
+    errorHandlingTests(apiCall);
+  });
+
+  describe('rename', () => {
+    const apiCall = () => rename('', '0', 'newName');
+
+    test('resolves on success', async () => {
+      fetchMock.mockResponseOnce(JSON.stringify({}));
+      await expect(apiCall()).resolves.toBeUndefined();
+    });
+
+    errorHandlingTests(apiCall);
+  });
+});
+
+export {};

src/setupTests.ts
@@ -2,8 +2,9 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-// eslint-disable-next-line import/no-extraneous-dependencies
+/* eslint-disable import/no-extraneous-dependencies */
 import '@testing-library/jest-dom';
+import fetchMock from 'jest-fetch-mock';
 
 // mock i18next
 jest.mock('react-i18next', () => ({
@@ -16,3 +17,6 @@ jest.mock('react-i18next', () => ({
     },
   }),
 }));
+
+// mock fetch
+fetchMock.enableMocks();