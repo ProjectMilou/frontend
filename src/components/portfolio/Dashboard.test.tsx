f6545253f287edb3d580a3d6cad418c34a1ac3c6
an-p <80103802+an-p@users.noreply.github.com>
2021-03-25 13:07:32 +0100

    Rename portfolio (#69)
    * Add a button component with progress indicator.
    * Add a generic dialog component for name input.
    * Mock the react-i18next Trans component to allow testing.
    * Implement a dialog to rename portfolios.
    * Add rename dialog to the dashboard.
    * Change API URL.

--- src/components/portfolio/Dashboard.test.tsx
+++ src/components/portfolio/Dashboard.test.tsx
@@ -1,4 +1,10 @@
-import { render, fireEvent, act, waitFor } from '@testing-library/react';
+import {
+  render,
+  fireEvent,
+  act,
+  waitFor,
+  screen,
+} from '@testing-library/react';
 import * as React from 'react';
 import * as API from '../../portfolio/APIClient';
 import Dashboard, { DashboardProps } from './Dashboard';
@@ -91,5 +97,49 @@ describe('Dashboard', () => {
     expect(queryByText(MockOverview.name)).toBeInTheDocument();
     expect(queryByText(MockOverviewTwo.name)).toBeInTheDocument();
     expect(queryByText('portfolio.dashboard.createPortfolio')).not.toBeNull();
+  });
+
+  test('rename popup renames the portfolio', async () => {
+    const mockPortfolioOverview = mockAPI.list.mockResolvedValue([
+      MockOverview,
+      MockOverviewTwo,
+    ]);
+    mockAPI.rename.mockResolvedValue();
+    const { container } = renderComponent();
+    await act(async () => {
+      // wait until the component is rendered
+      await waitFor(() => {
+        expect(mockPortfolioOverview).toHaveBeenCalledTimes(1);
+      });
+    });
+
+    // no popup visible yet
+    expect(screen.queryByText('portfolio.dialog.rename.title')).toBeNull();
+
+    // popup opens on click and shows the correct name
+    fireEvent.click(container.querySelectorAll('button svg')[2]);
+    expect(
+      screen.queryByText('portfolio.dialog.rename.title')
+    ).toBeInTheDocument();
+    expect(document.querySelector('input')).toHaveValue(MockOverview.name);
+
+    // rename the portfolio and close the popup
+    fireEvent.change(document.querySelector('input')!, {
+      target: { value: 'new name' },
+    });
+    fireEvent.click(screen.getByText('portfolio.rename'));
+    await waitFor(() => {
+      expect(screen.queryByText('portfolio.dialog.rename.title')).toBeNull();
+    });
+    expect(screen.queryByText(MockOverview.name)).toBeNull();
+    expect(screen.queryByText('new name')).toBeInTheDocument();
+
+    // popup shows the correct name for a different portfolio
+    // popup opens on click and shows the correct name
+    fireEvent.click(container.querySelectorAll('button svg')[5]);
+    expect(
+      screen.queryByText('portfolio.dialog.rename.title')
+    ).toBeInTheDocument();
+    expect(document.querySelector('input')).toHaveValue(MockOverviewTwo.name);
   });
 });
---
+++ src/components/portfolio/ProgressButton.tsx
@@ -0,0 +1,67 @@
+// adapted from https://github.com/mui-org/material-ui/blob/f285808fbf8728b0cc0435e65a76dbc691be3643/docs/src/pages/components/progress/CircularIntegration.tsx
+
+import {
+  Button,
+  CircularProgress,
+  makeStyles,
+  PropTypes,
+} from '@material-ui/core';
+import React, { MouseEventHandler } from 'react';
+
+export type ProgressButtonProps = {
+  onClick: MouseEventHandler;
+  disabled?: boolean;
+  loading?: boolean;
+  color?: PropTypes.Color;
+};
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+  },
+  wrapper: {
+    margin: theme.spacing(1),
+    position: 'relative',
+  },
+  buttonProgress: {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    marginTop: '-12px',
+    marginLeft: '-12px',
+  },
+}));
+
+/**
+ * A button with a circular progress indicator.
+ *
+ * @param onClick - Click event handler
+ * @param disabled - Whether the button is disabled. Note: The button is always disabled if {@param loading} is set.
+ * @param loading - Whether the loading indicator should be displayed
+ * @param color - The color of the button
+ * @param children - The content of the button
+ */
+const ProgressButton: React.FC<ProgressButtonProps> = ({
+  onClick,
+  disabled,
+  loading,
+  color,
+  children,
+}) => {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <div className={classes.wrapper}>
+        <Button disabled={loading || disabled} onClick={onClick} color={color}>
+          {children}
+        </Button>
+        {loading && (
+          <CircularProgress size={24} className={classes.buttonProgress} />
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default ProgressButton;
