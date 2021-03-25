1e683637367280defc27c1cc93cc506685e620a2
an-p <80103802+an-p@users.noreply.github.com>
2021-03-25 13:33:39 +0100

    CI: Run eslint on all files, enforce prettier code style (#85)
    * Add prettier scripts and workflow.
    * Run eslint directly.
    * Fix code style.

--- src/components/analyser/Analyser.tsx
+++ src/components/analyser/Analyser.tsx
@@ -1,6 +1,8 @@
 import { RouteComponentProps } from '@reach/router';
 import React from 'react';
 
-const Analyser: React.FC<RouteComponentProps> = () => <div>Analyser component</div>;
+const Analyser: React.FC<RouteComponentProps> = () => (
+  <div>Analyser component</div>
+);
 
 export default Analyser;
