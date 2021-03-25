0e6d21afe7caad283972b0f22c8e4ccb7ff67a0a
Fanxin YANG <fanxin.yang@tum.de>
2021-03-25 22:52:40 +0100

    impove buttons

--- src/components/analyser/search/Filter.tsx
+++ src/components/analyser/search/Filter.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable no-param-reassign */
 import React from 'react';
 import { createStyles, makeStyles, Theme, useTheme } from '@material-ui/core/styles';
-import { Input, InputLabel, MenuItem, FormControl, ListItemText, Select, Checkbox, Chip, Button, Typography, Container } from '@material-ui/core';
+import { Input, InputLabel, MenuItem, FormControl, ListItemText, Select, Checkbox, Chip, Button, ButtonGroup } from '@material-ui/core';
 import { FilterList, Delete } from '@material-ui/icons';
 import { useTranslation } from 'react-i18next';
 import * as API from '../../../analyser/APIClient';
@@ -144,7 +144,7 @@ const Filteroptions: React.FC<FilterOptionsProps> = ({
     const handleChange = (event: React.ChangeEvent<{ name?: string | undefined, value: unknown }>) => {
         const temp = event.target.value as string[]
         const name = event.target.name as string
-        if (temp.indexOf("") > -1){
+        if (temp.indexOf("") > -1) {
             switch (name) {
                 case 'Country':
                     setFilters(prevFilter => {
@@ -202,12 +202,14 @@ const Filteroptions: React.FC<FilterOptionsProps> = ({
             <FilterBar filtersList={filters.countries} ogFiltersList={ogFilters.countries} handleChange={handleChange} name={t('stock.country')} />
             <FilterBar filtersList={filters.industries} ogFiltersList={ogFilters.industries} handleChange={handleChange} name={t('stock.industry')} />
             <FilterBar filtersList={filters.currency} ogFiltersList={ogFilters.currency} handleChange={handleChange} name={t('stock.currency')} />
-            <Button type='button' onClick={() => sendRequest} startIcon={<FilterList />}>
-                Filter
-            </Button>
-            <Button type='button' onClick={clearFilters} startIcon={<Delete />}>
-                Clear filters
-            </Button>
+            <ButtonGroup variant="contained" color='primary'>
+                <Button type='button' onClick={() => sendRequest} startIcon={<FilterList />}>
+                    Adapt
+                </Button>
+                <Button type='button' onClick={clearFilters} startIcon={<Delete />}>
+                    Clear All
+                </Button>
+            </ButtonGroup>
         </>
     )
 };
