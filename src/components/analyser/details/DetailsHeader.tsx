1c2bd72eb8248f5adcd0eaf6203da775747becc6
Timothy Summers <timmy.summers@hotmail.de>
2021-03-27 17:33:44 +0100

    Added 1day 7day and 30 day price info to header

--- src/components/analyser/details/DetailsHeader.tsx
+++ src/components/analyser/details/DetailsHeader.tsx
@@ -3,6 +3,7 @@ import { Container, makeStyles, Typography } from '@material-ui/core';
 import { symbol } from 'prop-types';
 import * as API from '../../../analyser/APIClient';
 import EuroCurrency from '../EuroCurrency';
+import Performance from '../Performance';
 
 export type DetailsProps = {
     details: API.Stock;
@@ -21,7 +22,13 @@ const useStyles = makeStyles({
     },
     price: {
       paddingLeft: '6'
-    }
+    },
+    wknIsin: {
+      'font-size': '12px',
+      color: '#EEF1FB',
+      minWidth: '50%',
+      maxWidth: '1000px',
+    },
   });
 
   const DetailsHeader: React.FC<DetailsProps> = ({details}) => {
@@ -30,8 +37,17 @@ const useStyles = makeStyles({
       <div className={classes.header}>
         <Container maxWidth="lg">
           <Typography className={classes.text}>
-          {details.name}
+          {details.name}<>&emsp;&emsp;&emsp;</>
            <EuroCurrency value={details.price} size='35px'/>
+           <>&emsp;&emsp;</>
+           <Performance value={details['1d']} />
+           <>&emsp;&emsp;</>
+           <Performance value={details['7d']} />
+           <>&emsp;&emsp;</>
+           <Performance value={details['30d']} />
+           <Typography className={classes.wknIsin}>
+           <>WKN: </>{details.WKN}<> / ISIN: </>{details.ISIN}
+           </Typography>
           </Typography>
         </Container>
       </div>
