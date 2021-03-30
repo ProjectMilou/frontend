46c6cd20eae04be6951d3da59807b1763a69865f
Ece Ata <eceatata@hotmail.com>
2021-03-30 09:39:44 +0200

    card width changed

--- src/components/analyser/search/DashboardCards.tsx
+++ src/components/analyser/search/DashboardCards.tsx
@@ -30,8 +30,8 @@ const useStyles = makeStyles((theme: Theme) => ({
     cursor: 'not-allowed',
   },
   card: {
-    width: 300,
-    margin: 20,
+    width: 280,
+    margin: 10,
     transition: '0.3s',
     boxShadow: '0 8px 40px -12px rgba(0,0,0,0.3)',
     '&:hover': {
