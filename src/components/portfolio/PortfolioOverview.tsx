f55808f7abe4c58e0ed3f98d7de86b8cd0686131
ge75bip <julianbouchard1@gmail.com>
2021-04-04 20:52:53 +0200

    Linter fix

--- src/components/portfolio/PortfolioOverview.tsx
+++ src/components/portfolio/PortfolioOverview.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
-import { makeStyles, Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core';
 import * as API from '../../portfolio/APIClient';
 import DashboardViewSelect, {
   DashboardView,
