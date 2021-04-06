f257b34d96f988e9052e7c43e16d8dd7eceab656
Wiebke Freitag <wiebke.freitag@satellytes.com>
2021-04-06 17:35:21 +0200

    Merge branch 'main' of /Users/wiebkefreitag/Documents/frontend with conflicts.

---
+++ src/components/shell/register/RegisterFailedWindow.test.tsx
@@ -0,0 +1,11 @@
+import { render, screen } from '@testing-library/react';
+import * as React from 'react';
+import RegisterFailedWindow from './RegisterFailedWindow';
+
+describe('RegisterFailedWindow', () => {
+  // TODO: Write actual tests
+  test('example test', () => {
+    // render(<RegisterFailedWindow />);
+    // expect(screen.getByText(/portfolio/i)).toBeInTheDocument();
+  });
+});
