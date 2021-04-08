4cb6e5649148839f1a5c161bc9b730b6f3d04f8d
MaxSchaTUM <schaller@in.tum.de>
2021-04-08 14:11:43 +0200

    refactored DetailsMainBacktestingTimeline.tsx by adding DetailsMainBacktestingTimelineItem.tsx for reuse and better readability (Date type still needs to be replaced instead of string everywhere else)

--- src/components/portfolio/DetailsMainBacktestingTimeline.tsx
+++ src/components/portfolio/DetailsMainBacktestingTimeline.tsx
@@ -2,51 +2,16 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { useTranslation } from 'react-i18next';
 import Timeline from '@material-ui/lab/Timeline';
-import TimelineItem from '@material-ui/lab/TimelineItem';
-import TimelineSeparator from '@material-ui/lab/TimelineSeparator';
-import TimelineConnector from '@material-ui/lab/TimelineConnector';
-import TimelineContent from '@material-ui/lab/TimelineContent';
-import TimelineOppositeContent from '@material-ui/lab/TimelineOppositeContent';
-import TimelineDot from '@material-ui/lab/TimelineDot';
 import EuroSymbolIcon from '@material-ui/icons/EuroSymbol';
 import TodayIcon from '@material-ui/icons/Today';
 import EventAvailableIcon from '@material-ui/icons/EventAvailable';
-import Paper from '@material-ui/core/Paper';
-import Typography from '@material-ui/core/Typography';
 import EuroCurrency from '../shared/EuroCurrency';
+import DetailsMainBacktestingTimelineItem from './DetailsMainBacktestingTimelineItem';
 
 const useStyles = makeStyles((theme) => ({
   timeline: {
     flexGrow: 0,
     flexBasis: '50%',
-  },
-  paper: {
-    padding: '6px 16px',
-    backgroundColor: theme.palette.primary.light,
-    color: theme.palette.primary.contrastText,
-  },
-  timelineItem: {
-    marginTop: '10px',
-    marginBottom: '10px',
-    display: 'flex',
-    justifyContent: 'center',
-  },
-  oppositeContent: {
-    color: theme.palette.primary.contrastText,
-    flexGrow: 0,
-    margin: 0,
-    flexBasis: '30%',
-    whiteSpace: 'nowrap',
-  },
-  timelineContent: {
-    flexGrow: 0,
-    flexBasis: '30%',
-    whiteSpace: 'nowrap',
-  },
-  timelineConnector: {
-    position: 'absolute',
-    height: '2.5rem',
-    top: '80%',
   },
   minValue: {
     color: theme.palette.success.light,
@@ -65,13 +30,13 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 type DetailsMainBacktestingTimelineProps = {
-  startDate: string;
+  startDate: Date;
   startValue: number;
-  minDate: string;
+  minDate: Date;
   minVale: number;
-  maxDate: string;
+  maxDate: Date;
   maxValue: number;
-  endDate: string;
+  endDate: Date;
   endValue: number;
 };
 
@@ -90,89 +55,30 @@ const DetailsMainBacktestingTimeline: React.FC<DetailsMainBacktestingTimelinePro
 
   return (
     <Timeline align="left" className={classes.timeline}>
-      <TimelineItem className={classes.timelineItem}>
-        <TimelineOppositeContent className={classes.oppositeContent}>
-          <Typography>{startDate}</Typography>
-        </TimelineOppositeContent>
-        <TimelineSeparator>
-          <TimelineDot color="primary">
-            <TodayIcon className={classes.startAndEnd} />
-          </TimelineDot>
-          <TimelineConnector className={classes.timelineConnector} />
-        </TimelineSeparator>
-        <TimelineContent className={classes.timelineContent}>
-          <Paper elevation={3} className={classes.paper}>
-            <Typography variant="h6" component="h1">
-              {t('portfolio.details.backtesting.start')}
-            </Typography>
-            <Typography>
-              <EuroCurrency value={startValue} size="1em" />
-            </Typography>
-          </Paper>
-        </TimelineContent>
-      </TimelineItem>
-      <TimelineItem className={classes.timelineItem}>
-        <TimelineOppositeContent className={classes.oppositeContent}>
-          <Typography>{minDate}</Typography>
-        </TimelineOppositeContent>
-        <TimelineSeparator>
-          <TimelineDot color="primary">
-            <EuroSymbolIcon className={classes.euroMin} />
-          </TimelineDot>
-          <TimelineConnector className={classes.timelineConnector} />
-        </TimelineSeparator>
-        <TimelineContent className={classes.timelineContent}>
-          <Paper elevation={3} className={classes.paper}>
-            <Typography variant="h6" component="h1">
-              {t('portfolio.details.backtesting.min')}
-            </Typography>
-            <Typography>
-              <EuroCurrency value={minVale} size="1em" />
-            </Typography>
-          </Paper>
-        </TimelineContent>
-      </TimelineItem>
-      <TimelineItem className={classes.timelineItem}>
-        <TimelineOppositeContent className={classes.oppositeContent}>
-          <Typography>{maxDate}</Typography>
-        </TimelineOppositeContent>
-        <TimelineSeparator>
-          <TimelineDot color="primary">
-            <EuroSymbolIcon className={classes.euroMax} />
-          </TimelineDot>
-          <TimelineConnector className={classes.timelineConnector} />
-        </TimelineSeparator>
-        <TimelineContent className={classes.timelineContent}>
-          <Paper elevation={3} className={classes.paper}>
-            <Typography variant="h6" component="h1">
-              {t('portfolio.details.backtesting.max')}
-            </Typography>
-            <Typography>
-              <EuroCurrency value={maxValue} size="1em" />{' '}
-            </Typography>
-          </Paper>
-        </TimelineContent>
-      </TimelineItem>
-      <TimelineItem className={classes.timelineItem}>
-        <TimelineOppositeContent className={classes.oppositeContent}>
-          <Typography>{endDate}</Typography>
-        </TimelineOppositeContent>
-        <TimelineSeparator>
-          <TimelineDot color="primary">
-            <EventAvailableIcon className={classes.startAndEnd} />
-          </TimelineDot>
-        </TimelineSeparator>
-        <TimelineContent className={classes.timelineContent}>
-          <Paper elevation={3} className={classes.paper}>
-            <Typography variant="h6" component="h1">
-              {t('portfolio.details.backtesting.end')}
-            </Typography>
-            <Typography>
-              <EuroCurrency value={endValue} size="1em" />
-            </Typography>
-          </Paper>
-        </TimelineContent>
-      </TimelineItem>
+      <DetailsMainBacktestingTimelineItem
+        date={startDate}
+        icon={<TodayIcon className={classes.startAndEnd} />}
+        title={t('portfolio.details.backtesting.start')}
+        value={<EuroCurrency value={startValue} size="1em" />}
+      />
+      <DetailsMainBacktestingTimelineItem
+        date={minDate}
+        icon={<EuroSymbolIcon className={classes.euroMin} />}
+        title={t('portfolio.details.backtesting.min')}
+        value={<EuroCurrency value={minVale} size="1em" />}
+      />
+      <DetailsMainBacktestingTimelineItem
+        date={maxDate}
+        icon={<EuroSymbolIcon className={classes.euroMax} />}
+        title={t('portfolio.details.backtesting.max')}
+        value={<EuroCurrency value={maxValue} size="1em" />}
+      />
+      <DetailsMainBacktestingTimelineItem
+        date={endDate}
+        icon={<EventAvailableIcon className={classes.startAndEnd} />}
+        title={t('portfolio.details.backtesting.end')}
+        value={<EuroCurrency value={endValue} size="1em" />}
+      />
     </Timeline>
   );
 };
---
+++ src/components/portfolio/DetailsMainBacktestingTimelineItem.tsx
@@ -0,0 +1,79 @@
+import React, { ReactNode } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import TimelineOppositeContent from '@material-ui/lab/TimelineOppositeContent';
+import Typography from '@material-ui/core/Typography';
+import TimelineSeparator from '@material-ui/lab/TimelineSeparator';
+import TimelineDot from '@material-ui/lab/TimelineDot';
+import TimelineConnector from '@material-ui/lab/TimelineConnector';
+import TimelineContent from '@material-ui/lab/TimelineContent';
+import Paper from '@material-ui/core/Paper';
+import TimelineItem from '@material-ui/lab/TimelineItem';
+
+const useStyles = makeStyles((theme) => ({
+  timelineItem: {
+    marginTop: '10px',
+    marginBottom: '10px',
+    display: 'flex',
+    justifyContent: 'center',
+  },
+  oppositeContent: {
+    color: theme.palette.primary.contrastText,
+    flexGrow: 0,
+    margin: 0,
+    flexBasis: '30%',
+    whiteSpace: 'nowrap',
+  },
+  timelineContent: {
+    flexGrow: 0,
+    flexBasis: '30%',
+    whiteSpace: 'nowrap',
+  },
+  timelineConnector: {
+    position: 'absolute',
+    height: '2.5rem',
+    top: '80%',
+  },
+  paper: {
+    padding: '6px 16px',
+    backgroundColor: theme.palette.primary.light,
+    color: theme.palette.primary.contrastText,
+  },
+}));
+
+type DetailsMainBacktestingTimelineItemProps = {
+  date: Date;
+  icon: ReactNode;
+  title: string;
+  value: ReactNode;
+};
+
+const DetailsMainBacktestingTimelineItem: React.FC<DetailsMainBacktestingTimelineItemProps> = ({
+  date,
+  icon,
+  title,
+  value,
+}) => {
+  const classes = useStyles();
+
+  return (
+    <TimelineItem className={classes.timelineItem}>
+      <TimelineOppositeContent className={classes.oppositeContent}>
+        <Typography>{date.toLocaleDateString()}</Typography>
+      </TimelineOppositeContent>
+      <TimelineSeparator>
+        <TimelineDot color="primary">{icon}</TimelineDot>
+        <TimelineConnector className={classes.timelineConnector} />
+      </TimelineSeparator>
+      <TimelineContent className={classes.timelineContent}>
+        <Paper elevation={3} className={classes.paper}>
+          <Typography variant="h6" component="h1">
+            {title}
+          </Typography>
+          <Typography>{value}</Typography>
+        </Paper>
+      </TimelineContent>
+    </TimelineItem>
+  );
+};
+
+export default DetailsMainBacktestingTimelineItem;
