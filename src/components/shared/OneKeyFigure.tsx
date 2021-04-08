505db9c4a23ca84aaede03ff37600e2923992b00
Ece Ata <eceatata@hotmail.com>
2021-04-08 12:05:05 +0200

    lint fixed

--- src/components/shared/OneKeyFigure.tsx
+++ src/components/shared/OneKeyFigure.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
 import { Checkbox, Toolbar, FormControlLabel } from '@material-ui/core';
 import InfoButton from './InfoButton';
 
