111bed55a6a723242269c05f60b927be7cc4d261
Timothy Summers <timmy.summers@hotmail.de>
2021-04-09 14:14:35 +0200

    Balance Sheet initial commit
    ApexChart treemap problem

---
+++ src/components/analyser/details/BalanceSheetInfo.tsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import Chart from 'react-apexcharts';
+
+export type Series = {
+  name: string;
+  data: number[];
+};
+
+
+const BalanceSheetInfo: React.FC = (
+) => {
+  const options = {
+      series: [
+        {
+          data: [
+            {
+              x: "New Delhi",
+              y: 218,
+            },
+            {
+              x: "Kolkata",
+              y: 149,
+            },
+            {
+              x: "Mumbai",
+              y: 184,
+            },
+            {
+              x: "Ahmedabad",
+              y: 55,
+            },
+            {
+              x: "Bangaluru",
+              y: 84,
+            },
+            {
+              x: "Pune",
+              y: 31,
+            },
+            {
+              x: "Chennai",
+              y: 70,
+            }
+          ],
+        },
+      ],
+      legend: {
+        show: false
+      },
+      chart: {
+        height: 350,
+        type: 'treemap'
+      },
+      title: {
+        text: 'Basic Treemap'
+      },
+  }; 
+  
+  
+  return (
+    <div>
+      <Chart
+        options={options}
+      />
+
+    </div>
+  );
+};
+
+export default BalanceSheetInfo;
