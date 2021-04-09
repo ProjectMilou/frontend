765f2574fcf9eaa268d476a0ced0b91b5341d9f8
MaxSchaTUM <schaller@in.tum.de>
2021-04-09 09:51:12 +0200

    added docstring to explain the use of StyledNumberFormat.tsx

--- src/components/shared/StyledNumberFormat.tsx
+++ src/components/shared/StyledNumberFormat.tsx
@@ -2,6 +2,23 @@ import React from 'react';
 import NumberFormat from 'react-number-format';
 import { makeStyles, Theme } from '@material-ui/core';
 
+/**
+ * Component for number formatting and optional styling
+ *
+ * @param value - number value that is being displayed
+ * @param suffix - suffix for the formatted number value, use autocompletion of your IDE!
+ * IMPORTANT: don't use curly braces around the suffix string
+ *    WRONG: suffix={'&nbsp;€'}
+ *    CORRECT: suffix='&nbsp;€'
+ * @param size - optional parameter to set the font size, if left out inherits from parent
+ * @param fontWeight - optional parameter to set the font weight, if left out inherits from parent
+ * @param paintJob - optional parameter to color the value:
+ *    - true will color green for positive values and red for negative values
+ *    - if a hex-string is passed (e.g. '#fff') the text will be painted in that color
+ *    - default/false is to inherit from parent (sets color to undefined)
+ * @param decimalSeperator - optional, default is ","
+ * @param thousandSeperator - optional, default is "."
+ */
 type StyledNumberFormatProps = {
   value: number;
   suffix: '&nbsp;€' | '&nbsp;%';
