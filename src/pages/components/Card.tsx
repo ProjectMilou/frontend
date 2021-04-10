fbe77bcf33a13eab9d51fe58516b1636293494cd
Sevval Palabiyik <sevvalpalabiyik@hotmail.com>
2021-04-10 11:44:51 -1100

    Images put into the same folder

--- src/pages/components/Card.tsx
+++ src/pages/components/Card.tsx
@@ -9,7 +9,7 @@ import {
   makeStyles,
 } from '@material-ui/core';
 
-import milou from '../media/milou.png';
+import milou from '../../assets/images/milou.png';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
