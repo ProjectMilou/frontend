afddac0808d32c5229ea8a7b5ed339044c8cd393
MaxSchaTUM <schaller@in.tum.de>
2021-04-11 20:15:30 +0200

    tried to implement algorithm for sorting data for display

--- src/components/portfolio/DetailsMainAnalyticsCorrelations.tsx
+++ src/components/portfolio/DetailsMainAnalyticsCorrelations.tsx
@@ -4,6 +4,63 @@ import { useTheme } from '@material-ui/core';
 import { useTranslation } from 'react-i18next';
 import { NonEmptyPortfolioDetails } from '../../portfolio/APIClient';
 
+type Correlation = {
+  [key: string]: number;
+};
+
+type MappedCorrelation = [string, string, number];
+
+type HeatMapMetric = {
+  name: string;
+  // the order of these correlation values are always the same, e.g. always BMW first, then Apple...
+  orderedValues: number[];
+};
+
+const categories = [
+  'BMW',
+  'Apple',
+  'J&J',
+  'TUM',
+  'Google',
+  'Dell',
+  'Nestle',
+  'Android',
+  'Faber',
+  'TripAdvisor',
+];
+
+const mockCorr: Correlation = {
+  'BMW;Apple': 0.397373186,
+  'Apple;TUM': 0.352196405,
+  'Faber;TUM': 0.57879282,
+};
+
+const mappedCorrs: MappedCorrelation[] = Object.keys(mockCorr).map((key) => [
+  key.split(';')[0],
+  key.split(';')[1],
+  mockCorr[key],
+]);
+
+categories.forEach((c) => {
+  const oneMetric: HeatMapMetric = {
+    name: c,
+    orderedValues: categories.map((c2) => {
+      if (c2 === c) return 1;
+      const matchingResults = mappedCorrs.filter(
+        // check if c, c2 (current name and current key of the orderedValue) are the same two values as
+        // in the ordered correlation received from backend => if yes the corresponding correlation value
+        (mc) =>
+          JSON.stringify([c, c2].sort()) ===
+          JSON.stringify(mc.slice(0, 2).sort())
+      );
+      if (matchingResults.length === 1) return matchingResults[0];
+      // in this case there was no match in the data from backend or more than one
+      // TODO proper error handling
+      return -1;
+    }),
+  };
+});
+
 // TODO replace with actual api values
 const mockCorrelations = [
   {
@@ -16,6 +73,10 @@ const mockCorrelations = [
       {
         x: 'Mercedes',
         y: 0.9,
+      },
+      {
+        x: 'MCLaren',
+        y: 0.6,
       },
     ],
   },
