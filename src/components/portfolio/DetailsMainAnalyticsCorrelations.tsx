e90f69485e17b9b8b7c709cc4cba41a5ea177856
MaxSchaTUM <schaller@in.tum.de>
2021-04-12 11:24:25 +0200

    added logic to construct series

--- src/components/portfolio/DetailsMainAnalyticsCorrelations.tsx
+++ src/components/portfolio/DetailsMainAnalyticsCorrelations.tsx
@@ -4,45 +4,17 @@ import { useTheme } from '@material-ui/core';
 import { useTranslation } from 'react-i18next';
 import { NonEmptyPortfolioDetails } from '../../portfolio/APIClient';
 
-type Correlation = {
+type Correlations = {
   [key: string]: number;
 };
 
 type MappedCorrelation = [string, string, number];
 
-const mockCorr: Correlation = {
-  'BMW;Apple': 0.397373186,
-  'Apple;TUM': 0.352196405,
-  'Faber;TUM': 0.57879282,
+type HeatMapSeries = {
+  name: string;
+  // the order of which number represents which company is always the same
+  orderedValues: number[];
 };
-
-const mappedCorrs: MappedCorrelation[] = Object.keys(mockCorr).map((key) => [
-  key.split(';')[0],
-  key.split(';')[1],
-  mockCorr[key],
-]);
-
-// neuerplan erstelle ein array von name, data objekten
-// lege reihenfolge fest für data (da immer gleichen firmen damit auf x achse nichts schief geht
-// iteriere durch mapped corrs durch und pushe in das array
-
-// TODO replace with actual api values
-const mockCorrelations = [
-  {
-    name: 'BMW',
-    data: [1, 5, 6],
-  },
-  {
-    name: 'Audi',
-    data: [4, 5, 9],
-  },
-  {
-    name: 'Mercedes',
-    data: [3, 5, 9],
-  },
-];
-
-const mockCategories = ['BMW', 'Audi', 'Mercedes'];
 
 type HeatmapProps = {
   portfolio: NonEmptyPortfolioDetails;
@@ -52,6 +24,59 @@ type HeatmapProps = {
 const Heatmap: React.FC<HeatmapProps> = ({ portfolio }) => {
   const theme = useTheme();
   const { t } = useTranslation();
+
+  // TODO replace with correlations from props (rework of API client necessary
+  const correlations: Correlations = {
+    'BMW;Apple': 1,
+    'Apple;TUM': 2,
+    'Apple;Faber': 3,
+    'TUM;BMW': 4,
+    'BMW;Faber': 5,
+    'Faber;TUM': 6,
+  };
+
+  // array of arrays of symbol - symbol - correlation
+  const mappedCorrelations: MappedCorrelation[] = Object.keys(
+    correlations
+  ).map((key) => [key.split(';')[0], key.split(';')[1], correlations[key]]);
+
+  // gets unique set of companies displayed in the chart (used as x-axis categories)
+  const chartCategories = new Set(
+    mappedCorrelations
+      .map((mc) => [mc[0], mc[1]])
+      .reduce((arr1, arr2) => arr1.concat(arr2))
+      .sort()
+  );
+
+  // will be passed to the chart as series prop
+  const finalSeries: HeatMapSeries[] = [];
+
+  chartCategories.forEach((c1) => {
+    const currentSeries: HeatMapSeries = {
+      name: c1,
+      orderedValues: [],
+    };
+    chartCategories.forEach((c2) => {
+      // the correlation of a company to itself is 1
+      if (c1 === c2) currentSeries.orderedValues.push(1);
+      else {
+        // check if c1 + c2 are present as a pair in the mappedCorrelations ignoring order
+        // if present push the corresponding correlation value, if no match is found place default value 0
+        // it is mandatory that each series has the same amount and order of ordered values to display a meaningful x-Axis in the cahrt
+        let valueToPush = 0;
+        mappedCorrelations.forEach((mc) => {
+          if (
+            JSON.stringify([c1, c2].sort()) ===
+            JSON.stringify(mc.slice(0, 2).sort())
+          )
+            // eslint-disable-next-line prefer-destructuring
+            valueToPush = mc[2];
+        });
+        currentSeries.orderedValues.push(valueToPush);
+      }
+    });
+    finalSeries.push(currentSeries);
+  });
 
   const options = {
     chart: {
@@ -69,7 +94,7 @@ const Heatmap: React.FC<HeatmapProps> = ({ portfolio }) => {
       },
     },
     xaxis: {
-      categories: mockCategories,
+      categories: chartCategories,
       labels: {
         style: {
           colors: theme.palette.primary.contrastText,
@@ -86,12 +111,7 @@ const Heatmap: React.FC<HeatmapProps> = ({ portfolio }) => {
   };
 
   return (
-    <Chart
-      type="heatmap"
-      height={350}
-      series={mockCorrelations}
-      options={options}
-    />
+    <Chart type="heatmap" height={350} series={finalSeries} options={options} />
   );
 };
 
