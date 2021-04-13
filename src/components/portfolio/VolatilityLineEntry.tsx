0965b7f4a54149ce5b03f1cc8e15f207e3fc32dd
Sihan Jennifer Zhao <sihan.jennifer.zhao@gmail.com>
2021-04-13 12:53:11 +0200

    Modify colors to palette colors, add preliminary text to component

--- src/components/portfolio/VolatilityLineEntry.tsx
+++ src/components/portfolio/VolatilityLineEntry.tsx
@@ -1,30 +1,44 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import { Tooltip } from '@material-ui/core';
 
 type VolatilityLineEntryProps = {
   volatilityValue: number;
+  tooltipText: string;
 };
 
 const VolatilityLineEntry: React.FC<VolatilityLineEntryProps> = ({
   volatilityValue,
+  tooltipText,
 }) => {
-  const useStyles = makeStyles({
-    line: {
-      position: 'absolute',
-      height: '20px',
-      width: '3px',
-      backgroundColor: 'white',
-    },
-  });
+  const useStyles = makeStyles(({ palette }: Theme) =>
+    createStyles({
+      line: {
+        position: 'absolute',
+        height: '20px',
+        width: '3px',
+        backgroundColor: palette.primary.contrastText,
+      },
+      tooltip: {
+        fontSize: '0.8rem',
+        fontWeight: 600,
+        lineHeight: '1.2rem',
+        whiteSpace: 'pre-line',
+      },
+    })
+  );
+
   const classes = useStyles();
 
   return (
-    <div
-      className={classes.line}
-      style={{
-        left: `${Math.round((volatilityValue / 2.0) * 1000) / 10}%`,
-      }}
-    />
+    <Tooltip title={<p className={classes.tooltip}>{tooltipText}</p>}>
+      <div
+        className={classes.line}
+        style={{
+          left: `${Math.round((volatilityValue / 2.0) * 1000) / 10}%`,
+        }}
+      />
+    </Tooltip>
   );
 };
 
