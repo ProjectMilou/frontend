08a1d0042df25602ef518959b427f66c0e50208e
Sihan Jennifer Zhao <sihan.jennifer.zhao@gmail.com>
2021-04-13 20:37:21 +0200

    Add static information of analytics page

--- src/components/portfolio/DetailsVolatilityGraph.tsx
+++ src/components/portfolio/DetailsVolatilityGraph.tsx
@@ -11,6 +11,8 @@ import Grid from '@material-ui/core/Grid';
 import { useTranslation } from 'react-i18next';
 import VolatilityLineEntry from './VolatilityLineEntry';
 
+const portfolioVolatility = 0.5208850412096532;
+
 const DetailsVolatilityGraph: React.FC = () => {
   const useStyles = makeStyles(({ palette }: Theme) =>
     createStyles({
@@ -55,11 +57,20 @@ const DetailsVolatilityGraph: React.FC = () => {
   const theme = useTheme();
   const classes = useStyles();
 
+  const volatility = {
+    Apple: 1.0304461557501514,
+    Google: 0.5496680733681532,
+    Amazon: 0.5993856976545141,
+    IBM: 0.5765889119578946,
+    'Alibaba group': 0.7140162113218156,
+    'JPMorgan Chase & Co.': 0.6486754881067324,
+  };
+
   return (
     <>
       <h1 className={classes.text}>{t('portfolio.details.volatility')}</h1>
       <h3 className={classes.text}>
-        {Math.round(0.5208850412096532 * 1000) / 1000}
+        {Math.round(portfolioVolatility * 1000) / 1000}
       </h3>
       <h2 className={classes.text}>
         {t('portfolio.details.volatilityMarket')}
