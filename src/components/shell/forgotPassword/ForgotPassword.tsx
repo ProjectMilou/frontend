99eac64e96eb2b6cd92863d99276841b6dcf87c1
Simon Huang <simon.huang@tum.de>
2021-04-13 15:39:28 +0200

    update user interface

--- src/components/shell/forgotPassword/ForgotPassword.tsx
+++ src/components/shell/forgotPassword/ForgotPassword.tsx
@@ -7,14 +7,14 @@ import {
   Typography,
   TextField,
   makeStyles,
+  createStyles,
   useTheme,
 } from '@material-ui/core';
 import ClearIcon from '@material-ui/icons/Clear';
-import Divider from '@material-ui/core/Divider';
-import { Visibility, VisibilityOff } from '@material-ui/icons';
 import { useTranslation } from 'react-i18next';
 import PropTypes from 'prop-types';
 import logo from '../../../assets/images/logo1.png';
+import { UserService } from '../../../services/UserService';
 
 /*
 const url = process.env.URL_API
@@ -22,37 +22,39 @@ if(!url){
   throw new Error("URL_API is missing")
 }
 */
-
-const useStyles = makeStyles({
-  dialog: {
-    backgroundColor: 'white',
-    borderRadius: '10px',
-    padding: '20px',
-    position: 'relative',
-  },
-  iconLogo: {
-    maxWidth: 150,
-    display: 'block',
-    margin: '10px auto',
-  },
-  iconClear: {
-    position: 'absolute',
-    right: '20px',
-    top: '20px',
-    cursor: 'pointer',
-  },
-  button: {
-    textDecoration: 'underline',
-    cursor: 'pointer',
-    backgroundColor: 'white',
-    border: 'none',
-    padding: '0px',
-    fontSize: '14px',
-    lineHeight: '1.5',
-    margin: '10px auto',
-    display: 'block',
-  },
-});
+const useStyles = makeStyles((theme) =>
+  createStyles({
+    dialog: {
+      backgroundColor: 'white',
+      borderRadius: theme.spacing(1),
+      padding: theme.spacing(2),
+      position: 'relative',
+    },
+    iconLogo: {
+      maxWidth: 150,
+      display: 'block',
+      margin: theme.spacing(1, 'auto'),
+    },
+    iconClear: {
+      position: 'absolute',
+      right: theme.spacing(2),
+      top: theme.spacing(2),
+      cursor: 'pointer',
+    },
+    errorMessage: {
+      color: theme.palette.error.main,
+      border: theme.spacing(0.125, `solid ${theme.palette.error.main}`),
+      padding: theme.spacing(0.5),
+      margin: theme.spacing(0, 3),
+    },
+    successMessage: {
+      color: theme.palette.success.main,
+      border: theme.spacing(0.125, `solid ${theme.palette.success.main}`),
+      padding: theme.spacing(0.5),
+      margin: theme.spacing(0, 3),
+    },
+  })
+);
 
 interface ForgotPasswordProps {
   closePopUp: () => void;
@@ -61,13 +63,27 @@ interface ForgotPasswordProps {
 const ForgotPassword: React.FC<ForgotPasswordProps> = (props) => {
   const { closePopUp } = props;
   const theme = useTheme();
+  const {
+    dialog,
+    iconLogo,
+    iconClear,
+    errorMessage,
+    successMessage,
+  } = useStyles(theme);
   const { t } = useTranslation();
-  const { dialog, iconLogo, iconClear, button } = useStyles(theme);
   const [email, setEmail] = useState('');
+  const [hasError, setError] = useState(false);
+  const [hasSuccess, setSuccess] = useState(false);
 
   const handleSubmit = () => {
-    // TODO send request to the api for new token and show new form fields
-    // to enter token and change password
+    if (email === '') {
+      setError(true);
+      return;
+    }
+
+    UserService.forgot(email)
+      .then(() => setSuccess(true))
+      .catch(() => setSuccess(false));
   };
 
   return (
@@ -83,17 +99,28 @@ const ForgotPassword: React.FC<ForgotPasswordProps> = (props) => {
       <img src={logo} alt="milou-logo" className={iconLogo} />
 
       <Box mt={5}>
-        <Typography variant="body1" align="center">
-          {t('shell.forgotPassword.reset')}
-        </Typography>
+        {hasError && (
+          <Typography variant="body1" className={errorMessage}>
+            {t('error.invalidEmail')} {t('error.retry')}
+          </Typography>
+        )}
+        {hasSuccess && (
+          <Typography variant="body1" className={successMessage}>
+            {t('shell.forgotPassword.tokenSent')}
+          </Typography>
+        )}
         <DialogContent>
+          <Typography variant="body1" align="center">
+            {t('shell.forgotPassword.reset')}
+          </Typography>
+
           <TextField
             id="email"
             label={t('shell.email')}
             type="text"
             fullWidth
             style={{ margin: '10px 0' }}
-            inputProps={{ 'data-testid': 'email' }}
+            onChange={(event) => setEmail(event.target.value)}
           />
         </DialogContent>
 
