9c6706dff8deac281f36259f43e3dc9293875a0f
MaxSchaTUM <schaller@in.tum.de>
2021-04-13 15:38:03 +0200

    *"deleted" DetailsMainRisk.tsx for the time being, rework on the way *fixing other components that were affected by changing the types / mocks in APIClient.ts and APIMocks.ts *finished DebtEquity component

--- src/components/portfolio/DetailsAnalyticsDebtEquityBar.tsx
+++ src/components/portfolio/DetailsAnalyticsDebtEquityBar.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import Chart from 'react-apexcharts';
+import { useTranslation } from 'react-i18next';
+import { useTheme } from '@material-ui/core';
 import { NonEmptyPortfolioDetails } from '../../portfolio/APIClient';
 
 type DetailsAnalyticsDebtEquityBarProps = {
@@ -11,38 +13,64 @@ const DetailsAnalyticsDebtEquityBar: React.FC<DetailsAnalyticsDebtEquityBarProps
   portfolio,
   height,
 }) => {
+  const { t } = useTranslation();
+  const theme = useTheme();
   const biggestTen = portfolio.positions
     .sort(
       (pos1, pos2) => pos2.qty * pos2.stock.price - pos1.qty * pos1.stock.price
     )
     .slice(0, 9);
 
+  const series = [
+    {
+      name: t('portfolio.details.analytics.debtEquity'),
+      data: biggestTen.map((pos) => pos.stock.debtEquity),
+    },
+  ];
+
   const options = {
     chart: {
       type: 'bar',
+      toolbar: {
+        show: false,
+      },
     },
+    colors: [theme.palette.lightBlue.main],
     plotOptions: {
       bar: {
-        borderRadius: 4,
+        borderRadius: 5,
         horizontal: true,
       },
     },
     dataLabels: {
       enabled: false,
     },
+    title: {
+      text: t('portfolio.details.analytics.debtEquity.title'),
+      style: {
+        color: theme.palette.primary.contrastText,
+      },
+    },
     xaxis: {
-      categories: ['alla'],
+      categories: biggestTen.map((pos) => pos.stock.symbol),
+      labels: {
+        style: {
+          colors: theme.palette.primary.contrastText,
+        },
+      },
+    },
+    yaxis: {
+      labels: {
+        style: {
+          colors: theme.palette.primary.contrastText,
+        },
+      },
     },
   };
 
   return (
     <>
-      <Chart
-        options={options}
-        series={biggestTen.map((pos) => pos.stock)}
-        type="bar"
-        height={height}
-      />
+      <Chart options={options} series={series} type="bar" height={height} />
     </>
   );
 };
