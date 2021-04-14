edfc51bd9d8b23f5c540dc962bcae99a8e6cc4c3
Sihan Jennifer Zhao <sihan.jennifer.zhao@gmail.com>
2021-04-14 13:22:07 +0200

    Prettier fix and removing unused variables and imports

--- src/components/portfolio/VolatilityLineEntry.tsx
+++ src/components/portfolio/VolatilityLineEntry.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 import { Tooltip } from '@material-ui/core';
 
 type VolatilityLineEntryProps = {
@@ -13,22 +13,20 @@ const VolatilityLineEntry: React.FC<VolatilityLineEntryProps> = ({
   tooltipText,
   color,
 }) => {
-  const useStyles = makeStyles(({ palette }: Theme) =>
-    createStyles({
-      line: {
-        position: 'absolute',
-        height: '2rem',
-        width: '0.25rem',
-        transform: 'translate(0%, -50%)',
-      },
-      tooltip: {
-        fontSize: '0.8rem',
-        fontWeight: 600,
-        lineHeight: '1.2rem',
-        whiteSpace: 'pre-line',
-      },
-    })
-  );
+  const useStyles = makeStyles({
+    line: {
+      position: 'absolute',
+      height: '2rem',
+      width: '0.25rem',
+      transform: 'translate(0%, -50%)',
+    },
+    tooltip: {
+      fontSize: '0.8rem',
+      fontWeight: 600,
+      lineHeight: '1.2rem',
+      whiteSpace: 'pre-line',
+    },
+  });
 
   const classes = useStyles();
 
