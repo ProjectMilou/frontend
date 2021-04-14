929ceef2da00f6103a8429471257099116eea100
Simon Huang <simon.huang@tum.de>
2021-04-14 16:21:40 +0200

    add user interface

--- src/pages/PasswordReset.tsx
+++ src/pages/PasswordReset.tsx
@@ -1,7 +1,28 @@
-import React from 'react';
-import { DialogContent, Container, TextField } from '@material-ui/core';
+import React, { useState } from 'react';
+import {
+  Box,
+  Button,
+  Container,
+  DialogActions,
+  DialogContent,
+  Paper,
+  Typography,
+  makeStyles,
+  createStyles,
+  useTheme,
+} from '@material-ui/core';
 import { RouteComponentProps } from '@reach/router';
+import PasswordField from '../components/shell/register/PasswordField';
+import { ErrorState, UserInput } from '../components/shell/utils';
 
+const useStyles = makeStyles((theme) =>
+  createStyles({
+    formular: {
+      marginTop: theme.spacing(10),
+      padding: theme.spacing(5, 3),
+    },
+  })
+);
 interface PasswordResetProps extends RouteComponentProps {
   id?: string;
   token?: string;
@@ -9,9 +30,45 @@ interface PasswordResetProps extends RouteComponentProps {
 
 const PasswordReset: React.FC<PasswordResetProps> = (props) => {
   const { id, token } = props;
+  const { formular } = useStyles(useTheme());
+
+  const [hasError, setError] = useState({
+    email: '',
+    password: '',
+    confirmPassword: '',
+  } as ErrorState);
+
+  const [login, setLogin] = useState<UserInput>({
+    email: '',
+    password: '',
+    confirmPassword: '',
+  });
+
   return (
-    <Container maxWidth="md">
-      <DialogContent>TODO add form fields</DialogContent>
+    <Container maxWidth="xs">
+      <Paper className={formular}>
+        <DialogContent>
+          <Typography variant="h5" align="center">
+            Please enter your new password
+          </Typography>
+          <PasswordField
+            hasError={hasError}
+            login={login}
+            handleChange={() => {}}
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button
+            type="button"
+            variant="contained"
+            color="primary"
+            fullWidth
+            onClick={() => {}}
+          >
+            submit
+          </Button>
+        </DialogActions>
+      </Paper>
     </Container>
   );
 };
