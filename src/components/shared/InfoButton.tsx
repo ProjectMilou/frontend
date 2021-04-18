6ed7677aebafa665d0332c59433a05b10a7081ff
MaxSchaTUM <schaller@in.tum.de>
2021-04-18 14:31:13 +0200

    added left margin for info button

--- src/components/shared/InfoButton.tsx
+++ src/components/shared/InfoButton.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
+import { makeStyles, createStyles } from '@material-ui/core/styles';
 import { InfoOutlined } from '@material-ui/icons';
 import { Tooltip } from '@material-ui/core';
 
@@ -7,17 +7,13 @@ export type InfoProps = {
   infotext: string;
 };
 
-const useStyles = makeStyles((theme: Theme) =>
+const useStyles = makeStyles(() =>
   createStyles({
-    button: {
-      margin: theme.spacing(1),
+    root: {
+      marginLeft: '0.25rem',
     },
     customSize: {
       maxWidth: 500,
-    },
-    root: {
-      margin: '25px auto',
-      minWidth: '50%',
     },
     infoText: {
       fontSize: 'medium',
@@ -29,13 +25,15 @@ const InfoButton: React.FC<InfoProps> = ({ infotext }) => {
   const classes = useStyles();
 
   return (
-    <Tooltip
-      title={infotext}
-      placement="top-start"
-      classes={{ tooltip: classes.customSize }}
-    >
-      <InfoOutlined className={classes.infoText} />
-    </Tooltip>
+    <span className={classes.root}>
+      <Tooltip
+        title={infotext}
+        placement="top-start"
+        classes={{ tooltip: classes.customSize }}
+      >
+        <InfoOutlined className={classes.infoText} />
+      </Tooltip>
+    </span>
   );
 };
 
