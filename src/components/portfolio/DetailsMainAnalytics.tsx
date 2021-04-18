68f0f0a85a928e0f39c0abcee75234e25fa2ad67
ge75bip <julianbouchard1@gmail.com>
2021-04-18 17:21:57 +0200

    Made the volatility graph reusable. Renamed and moved almost all files related to the graph. Added coloring options.

--- src/components/portfolio/DetailsMainAnalytics.tsx
+++ src/components/portfolio/DetailsMainAnalytics.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Theme } from '@material-ui/core';
-import { createStyles, makeStyles } from '@material-ui/core/styles';
+import { createStyles, makeStyles, useTheme } from '@material-ui/core/styles';
 import { useTranslation } from 'react-i18next';
 import { NonEmptyPortfolioDetails } from '../../portfolio/APIClient';
 import StyledNumberFormat from '../shared/StyledNumberFormat';
 import DetailsAnalyticsHeatmap from './DetailsMainAnalyticsHeatmap';
 import DetailsAnalyticsDebtEquityBar from './DetailsAnalyticsDebtEquityBar';
-import DetailsVolatilityGraph from './DetailsVolatilityGraph';
+import DetailsVolatilityGraph from '../shared/VolatilityGraph';
+import VolatilityGraphPersonalPortfolio from '../shared/LargeVolatilityLineEntry';
+import VolatilityLineEntry from './VolatilityLineEntry';
 
 const useStyles = makeStyles(({ palette }: Theme) =>
   createStyles({
@@ -26,11 +28,30 @@ type DetailsMainAnalyticsProps = {
   portfolio: NonEmptyPortfolioDetails;
 };
 
+type VolatilityLineData = {
+  [symbol: string]: number;
+};
+
 const DetailsMainAnalytics: React.FC<DetailsMainAnalyticsProps> = ({
   portfolio,
 }) => {
+  const { palette } = useTheme();
   const classes = useStyles();
   const { t } = useTranslation();
+
+  const sortedStocks: VolatilityLineData = {};
+
+  if (portfolio.positions) {
+    Object.values(portfolio.positions)
+      // sort by total value (largest to smallest)
+      .sort((a, b) => b.qty * b.stock.price - a.qty * a.stock.price)
+      // take the top 5
+      .slice(0, 4)
+      .map((p) => p.stock)
+      .forEach((s) => {
+        sortedStocks[s.symbol] = s.volatility;
+      });
+  }
 
   return (
     <>
@@ -63,10 +84,23 @@ const DetailsMainAnalytics: React.FC<DetailsMainAnalyticsProps> = ({
       <div className={classes.titleWrapper}>
         {t('portfolio.details.analytics.volatility.vsMarket')}
       </div>
-      <DetailsVolatilityGraph
-        positions={portfolio.positions}
-        portfolioVolatility={portfolio.analytics.volatility}
-      />
+      <DetailsVolatilityGraph color={palette.primary.contrastText}>
+        <VolatilityGraphPersonalPortfolio
+          volatilityValue={portfolio.analytics.volatility}
+          name={t(
+            'portfolio.details.analytics.volatility.myPortfolio'
+          ).toString()}
+          textColor={palette.primary.contrastText}
+        />
+        {Object.entries(sortedStocks).map(([symbol, volatility]) => (
+          <VolatilityLineEntry
+            key={symbol}
+            volatilityValue={volatility}
+            tooltipText={symbol}
+            color={volatility > 1 ? palette.error.main : palette.success.main}
+          />
+        ))}
+      </DetailsVolatilityGraph>
     </>
   );
 };
---
+++ src/components/shared/LargeVolatilityLineEntry.tsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import {
+  createStyles,
+  makeStyles,
+  Theme,
+  useTheme,
+} from '@material-ui/core/styles';
+
+type StyleProps = {
+  left: string;
+  textColor: string;
+  backgroundColor: string;
+};
+
+const useStyles = makeStyles<Theme, StyleProps>(() =>
+  createStyles({
+    volatilityValue: {
+      position: 'absolute',
+      left: (props) => props.left,
+      display: 'flex',
+      flexDirection: 'column',
+    },
+    line: {
+      height: '3rem',
+      width: '0.25rem',
+      backgroundColor: (props) => props.backgroundColor,
+    },
+    text: {
+      color: (props) => props.textColor,
+      marginTop: '0.5rem',
+      transform: 'translate(-50%, 0%)',
+    },
+  })
+);
+
+type VolatilityGraphPersonalPortfolioProps = {
+  volatilityValue: number;
+  name: string;
+  textColor: string;
+};
+
+const VolatilityGraphPersonalPortfolio: React.FC<VolatilityGraphPersonalPortfolioProps> = ({
+  volatilityValue,
+  name,
+  textColor,
+}) => {
+  const { palette } = useTheme();
+
+  const styleProps: StyleProps = {
+    left: `${Math.round((volatilityValue / 2.0) * 1000) / 10}%`,
+    textColor,
+    backgroundColor:
+      volatilityValue > 1 ? palette.error.main : palette.success.main,
+  };
+
+  const classes = useStyles(styleProps);
+
+  return (
+    <>
+      <div className={classes.volatilityValue}>
+        <div className={classes.line} />
+        <div className={classes.text}>{`${name}: ${volatilityValue}`}</div>
+      </div>
+    </>
+  );
+};
+
+export default VolatilityGraphPersonalPortfolio;
