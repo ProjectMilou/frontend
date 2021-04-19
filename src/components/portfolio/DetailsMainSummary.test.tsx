d2ba156e57f18727e41e809e1c7dae7ca5c9e698
Sihan Jennifer Zhao <sihan.jennifer.zhao@gmail.com>
2021-04-19 21:17:42 +0200

    Update snapshot and DetailsMainSummary test to comply with rounded scores

--- src/components/portfolio/DetailsMainSummary.test.tsx
+++ src/components/portfolio/DetailsMainSummary.test.tsx
@@ -6,7 +6,7 @@ import { MockDetails } from '../../portfolio/APIMocks';
 test('DetailsMainSummary renders correctly', () => {
   render(<DetailsMainSummary portfolio={MockDetails} />);
 
-  screen.getAllByText(MockDetails.overview.score, { exact: false });
+  screen.getAllByText(Math.round(MockDetails.overview.score), { exact: false });
   screen.getAllByText(MockDetails.overview.perf7d, { exact: false });
   screen.getAllByText(MockDetails.overview.perf1y, { exact: false });
   // due to the use of EuroCurrency the decimal separator is changed.
