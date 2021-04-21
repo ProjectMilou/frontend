d2556627f0f37efb3306be6a7600c8ce09014740
Ece Ata <eceatata@hotmail.com>
2021-04-21 08:47:54 +0200

    sub headers added (#309)

--- src/components/analyser/details/Risks.tsx
+++ src/components/analyser/details/Risks.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
-import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
-import { useTranslation } from 'react-i18next';
 import * as API from '../../../analyser/APIClient';
 import Volatility from './Volatility';
 import Leverage from './Leverage';
+import SectionDivider from '../../shared/SectionDivider';
 
 // props type declaration
 export type DetailsProps = {
@@ -14,64 +13,21 @@ export type DetailsProps = {
   risks: API.RiskList;
 };
 
-const useStyles = makeStyles(({ palette, typography }: Theme) =>
-  createStyles({
-    titleContainer: {
-      display: 'flex',
-      marginBottom: '2rem',
-    },
-    titleWrapper: {
-      marginRight: '1rem',
-    },
-    sectionTitle: {
-      margin: 0,
-      color: palette.primary.main,
-      // TODO use theme fontsize and weight
-      fontSize: '2.25rem',
-      fontWeight: typography.fontWeightRegular,
-      whiteSpace: 'nowrap',
-    },
-    lineWrapper: {
-      display: 'flex',
-      width: '100%',
-      borderColor: palette.primary.main,
-    },
-    line: {
-      width: '100%',
-      alignSelf: 'center',
-      paddingLeft: '2%',
-    },
-  })
-);
-
 const Risks: React.FC<DetailsProps> = ({
   stockOverview,
   stockDetails,
   companyReports,
   interestCoverages,
   risks,
-}) => {
-  const classes = useStyles();
-  const { t } = useTranslation();
-  return (
-    <div>
-      <div className={classes.titleContainer}>
-        <div className={classes.titleWrapper}>
-          <h2 className={classes.sectionTitle}>
-            {t('analyser.details.RiskHeader')}
-          </h2>
-        </div>
-        <div className={classes.lineWrapper}>
-          <hr className={classes.line} />
-        </div>
-      </div>
-      <Volatility details={stockDetails} risks={risks} />
-      <Leverage
-        stockOverview={stockOverview}
-        companyReports={companyReports}
-        interestCoverages={interestCoverages}
-      />
-    </div>
-  );
-};
+}) => (
+  <div>
+    <SectionDivider section="analyser.details.RiskHeader" />
+    <Volatility details={stockDetails} risks={risks} />
+    <Leverage
+      stockOverview={stockOverview}
+      companyReports={companyReports}
+      interestCoverages={interestCoverages}
+    />
+  </div>
+);
 export default Risks;
