e8cbbf73930caadfa0ea6f0fadf2f2b9a0135325
Wiebke Freitag <wiebke.freitag@satellytes.com>
2021-04-21 08:31:25 +0200

    feat: added footer tests

--- src/components/footer/Footer.test.tsx
+++ src/components/footer/Footer.test.tsx
@@ -1,13 +1,32 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import React from 'react';
 import Footer from './Footer';
 
 describe('Footer', () => {
-  // TODO: Write actual tests
-  test('Renders all buttons', () => {
+  test('should renders all buttons', () => {
     render(<Footer />);
-    expect(screen.getByText(/About us/i)).toBeInTheDocument();
-    expect(screen.getByText(/Mobile App/i)).toBeInTheDocument();
-    expect(screen.getByText(/Privacy Policy/i)).toBeInTheDocument();
+    expect(screen.getByText(/footer.imprint/i)).toBeInTheDocument();
+    expect(screen.getByText(/footer.mobileApp/i)).toBeInTheDocument();
+    expect(screen.getByText(/footer.privacy/i)).toBeInTheDocument();
+    expect(screen.getByText(/footer.aboutUs/i)).toBeInTheDocument();
+  });
+
+  test('should change urls', () => {
+    render(<Footer />);
+    const imprintLink = screen.getByText(/footer.imprint/i);
+    fireEvent.click(imprintLink);
+    expect(window.location.pathname).toBe('/imprint');
+
+    const mobileAppLink = screen.getByText(/footer.mobileApp/i);
+    fireEvent.click(mobileAppLink);
+    expect(window.location.pathname).toBe('/mobileapp');
+
+    const privacyLink = screen.getByText(/footer.privacy/i);
+    fireEvent.click(privacyLink);
+    expect(window.location.pathname).toBe('/privacy');
+
+    const aboutUsLink = screen.getByText(/footer.aboutUs/i);
+    fireEvent.click(aboutUsLink);
+    expect(window.location.pathname).toBe('/aboutus');
   });
 });
