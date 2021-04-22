ec1f06ed8cfa322f5c56e15e2fdcfd9b4d28f912
Timmy <62808178+timothymaxwellsummers@users.noreply.github.com>
2021-04-22 14:06:10 +0200

    Fix/picture component (#326)
    * created CompanyLogo Component
    
    * CompanyLogo component used in DetailsOverview
    
    * Added Company Logo Component in DashboardCards
    
    * deleted unneccessary div
    
    * pretty

---
+++ src/components/analyser/CompanyLogo.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Stock } from '../../analyser/APIClient';
+import logo from '../../assets/images/logo1.png';
+
+type LogoProps = {
+  stockOverview: Stock;
+  style: string;
+};
+
+const CompanyLogo: React.FC<LogoProps> = ({ stockOverview, style }) => {
+  if (stockOverview.picture.toString() !== '') {
+    return (
+      <img
+        className={style}
+        alt="Company Logo"
+        src={stockOverview.picture.toString()}
+      />
+    );
+  }
+  return <img className={style} alt="Company Logo" src={logo} />;
+};
+
+export default CompanyLogo;
