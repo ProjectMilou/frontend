699b6ad938eb85aa85e08802ff795d9cef10323b
MaxSchaTUM <schaller@in.tum.de>
2021-04-23 09:07:55 +0200

    made tooltip font size bigger

--- src/components/portfolio/LimitedLength.tsx
+++ src/components/portfolio/LimitedLength.tsx
@@ -1,6 +1,17 @@
 import React from 'react';
 import { Tooltip } from '@material-ui/core';
+import { createStyles, makeStyles } from '@material-ui/core/styles';
 import { limitLength } from '../../portfolio/Helper';
+
+const useStyles = makeStyles(() =>
+  createStyles({
+    tooltip: {
+      maxWidth: 500,
+      fontSize: '0.9rem',
+      whiteSpace: 'pre-line',
+    },
+  })
+);
 
 type LimitedLengthProps = {
   value: string | number;
@@ -9,11 +20,12 @@ type LimitedLengthProps = {
 
 const LimitedLength: React.FC<LimitedLengthProps> = ({ value, length }) => {
   const maxLength = length || 20;
+  const classes = useStyles();
 
   return (
     <>
       {value.toString().length > maxLength ? (
-        <Tooltip title={value}>
+        <Tooltip classes={{ tooltip: classes.tooltip }} title={value}>
           <span>{limitLength(value)}</span>
         </Tooltip>
       ) : (
