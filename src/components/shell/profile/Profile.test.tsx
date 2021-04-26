ff93b69d2c183cf5b244e531c279baccc1d751f1
denispaluca <denispaluca@gmail.com>
2021-04-26 15:20:25 +0200

    Remove useless import from profile test

--- src/components/shell/profile/Profile.test.tsx
+++ src/components/shell/profile/Profile.test.tsx
@@ -1,4 +1,4 @@
-import { act, fireEvent, render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import * as React from 'react';
 import Profile from './Profile';
 
