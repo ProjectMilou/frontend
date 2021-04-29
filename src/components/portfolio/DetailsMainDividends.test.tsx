5461c470244b8951ffd62ea0fcc0f0f0a4f68cac
ge75bip <julianbouchard1@gmail.com>
2021-04-29 15:00:09 +0200

    Added tests and comments to the dividends section.

---
+++ src/components/portfolio/DetailsMainDividends.test.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { ThemeProvider } from '@material-ui/core/styles';
+import DetailsMainDividends from './DetailsMainDividends';
+import { MockDetails } from '../../portfolio/APIMocks';
+import { theme } from '../App';
+
+test('Dividens section renders correctly', async () => {
+  const renderComponent = () =>
+    render(
+      <ThemeProvider theme={theme}>
+        <DetailsMainDividends portfolio={MockDetails} />
+      </ThemeProvider>
+    );
+
+  const { container } = renderComponent();
+
+  // check for div yield title
+  screen.getByText('portfolio.details.divYield');
+
+  // check for div payout title
+  screen.getByText('portfolio.details.divPayout');
+
+  // check for next date title
+  screen.getByText('portfolio.details.nextDate');
+  // check for correct next date value
+  screen.getByText(`${MockDetails.nextDividend.toISOString().split('T')[0]}`);
+
+  // check for both graphs
+  expect(
+    container.querySelectorAll('[class ^= makeStyles-chartContainer]')
+      .length === 2
+  ).toBeTruthy();
+});
