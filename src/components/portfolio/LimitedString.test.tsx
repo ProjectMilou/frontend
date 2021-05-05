26befc39f394d466dea4ffd3379f0463d5c807a6
MaxSchaTUM <schaller@in.tum.de>
2021-05-05 17:27:13 +0200

    added test for LimitedString and NoInfoAvailable

---
+++ src/components/portfolio/LimitedString.test.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { act, render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import LimitedString from './LimitedString';
+
+describe('LimitedString', () => {
+  test('does not change string with less length', () => {
+    const limitedString = render(
+      <LimitedString value="This is a short string" />
+    );
+    expect(
+      limitedString.getByText('This is a short string')
+    ).toBeInTheDocument();
+  });
+  test('limits the length correctly', async () => {
+    const longText =
+      'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.';
+    const limitedString = render(
+      <LimitedString value={longText} length={15} />
+    );
+    const shortText = limitedString.getByText(
+      `${longText.substring(0, 16)}...`
+    );
+    expect(shortText).toBeInTheDocument();
+    act(() => {
+      userEvent.hover(shortText);
+    });
+    const tooltipText = await limitedString.findByText(longText);
+    expect(tooltipText).toBeInTheDocument();
+  });
+});
